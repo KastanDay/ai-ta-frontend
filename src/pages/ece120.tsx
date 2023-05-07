11aca14f1331fc57493727c1943a510e172469c5
Kastan Day <kastanvday@gmail.com>
2023-05-07 01:18:46 -0500

    added chat interface

--- src/pages/ece120.tsx
+++ src/pages/ece120.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import { Textarea, Card, Image, Text, Title, Badge, MantineProvider, Button, Group } from '@mantine/core';
+import { Textarea, Card, Image, Text, Title, Badge, MantineProvider, Button, Group, Stack } from '@mantine/core';
 
 import { FileInput, rem } from '@mantine/core';
 import { IconUpload } from '@tabler/icons-react';
@@ -39,19 +39,22 @@ const CourseMain: NextPage = () => {
           <h2 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             ECE <span className="text-[hsl(280,100%,70%)]">120</span>
           </h2>
-        <Text style={{fontFamily: 'Lora'}} size="md" color="white">
-          Taught by <Text style={{display: "inline"}} color="skyblue">Prof. Volodymyr (Vlad) Kindratenko</Text>, Director of the Center for Artificial Intelligence Innovation at NCSA, in <Text style={{display: "inline"}} color="skyblue">Spring 2022</Text>.
-        </Text>
+          <Text style={{fontFamily: 'Montserrat'}} size="md" color="white">
+            Taught by <Text style={{display: "inline"}} color="skyblue">Prof. Volodymyr (Vlad) Kindratenko</Text>, Director of the Center for Artificial Intelligence Innovation at NCSA, in <Text style={{display: "inline"}} color="skyblue">Spring 2022</Text>.
+          </Text>
+        </div>
         {/* <FileUpload /> */}
       
         {/* QA goes here */}
-        <Container size="100rem" px="md" py="md">
+        <Container size="100rem" px="md" py="lg" >
+          <AShortChat />
           <InputWithButton pb="md"/>
           <ChatSettings />
         </Container>
 
         {/* MAIN WEEKLY CONTENT */}
         {/* contentEditable="true" */}
+        <Container size="130rem" px="md" py="md">
         <Title order={1}>Course Overview</Title> 
         <div className="flex-container">
           <div className="item"><MaterialsCard /></div>
@@ -85,7 +88,9 @@ const CourseMain: NextPage = () => {
           variant="unstyled"
           size="xl"
           withAsterisk
-        />
+          />
+        
+        </Container>
 
 
 
@@ -96,7 +101,6 @@ const CourseMain: NextPage = () => {
         <Text STYLE="font-family: 'Montserrat'" ta="left" size="xl" weight="800">ECE 120</Text>
         <Title order={1}>ECE 120</Title>
         <Title align="left" order={2}>ECE 120</Title> */}
-        </div>
       </main>
     </>
   );
@@ -122,6 +126,69 @@ function ChatSettings() {
       <PublicChatSwitch />
       <UseGPT4Switch />
     </Flex>
+  );
+}
+
+function AShortChat() {
+  return (
+    <Container size="50rem" px="md" py="md" >
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Anakin
+          <time className="text-xs opacity-50">  12:46</time>
+        </div>
+        <div className="chat-bubble">What is a Finite State machine?</div>
+        <div className="chat-footer opacity-50">
+          Seen at 12:46
+        </div>
+      </div>
+
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Obi-Wan Kenobi
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
+        <div className="chat-bubble to-blue-600">A Finite State Machine (FSM) is a mathematical model used to represent and analyze systems that can be in a finite number of states and can transition between these states in response to external inputs. It is also known as a Finite Automaton or a State Transition System.</div>
+        <div className="chat-footer opacity-50 to-blue-600">
+          Delivered
+        </div>
+      </div>
+      <Text size="lg" weight={800} p={6}>From the course</Text>
+      <Group variant="row" spacing="xs">
+        {/* <MaterialsCard /> */}
+        {/* <MaterialsCard /> */}
+        <MaterialsCardSmall />
+        <MaterialsCardSmall />
+      </Group>
+
+
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Anakin
+          <time className="text-xs opacity-50">  12:47</time>
+        </div>
+        <div className="chat-bubble">Why are they useful??</div>
+        <div className="chat-footer opacity-50">
+          Seen at 12:47
+        </div>
+      </div>
+      
+    </Container>
   );
 }
 
@@ -284,6 +351,38 @@ function MaterialsCard() {
       <Button variant="light" color="blue" fullWidth mt="md" radius="md">
         View
       </Button>
+    </Card>
+    </div>
+  );
+}
+
+// In the chat box
+function MaterialsCardSmall() {
+  return (
+    <div className="box-sizing: border-box; border: 100px solid #ccc;">
+    <Card bg='#0E1116' style={{maxWidth: "20rem"}} shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+          height={"7rem"}
+          alt="Norway"
+          />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text style={{fontFamily: 'Montserrat'}} size="xl" weight={800}>Finite State Machine Readings</Text>
+        {/* <Badge size="xl" color="pink" variant="light">
+          ECE
+        </Badge> */}
+      </Group>
+
+      <Text size="sm" variant="gradient" weight={600} gradient={{ from: 'yellow', to: 'green', deg: 0 }} >
+        AI summary
+      </Text>
+      {/* style={{'font-family': 'Lora'}} */}
+      <Text className="fade" size="md" color="dimmed" > 
+        In a FSM, each state represents a specific condition or mode that the system can be in, and each transition represents a change of state triggered by a specific input or event. The FSM can be defined by a set of states, a set of input symbols or events, a set of output symbols or actions, and a transition function that maps each state and input to a next state and output.
+      </Text>
     </Card>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,6 +71,29 @@ const Home: NextPage = () => {
             </Link>
           </div>
         </div>
+
+        <div className="chat chat-start">
+          <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50">2 hours ago</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">
+            Seen
+          </div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50">2 hour ago</time>
+          </div>
+          <div className="chat-bubble">I loved you.</div>
+          <div className="chat-footer opacity-50">
+            Delivered
+          </div>
+        </div>
+
+        
         
 
         {/* <Text STYLE="font-family: 'Lora'" ta="left" variant="gradient" size="xl" weight="800">ECE 120</Text>
