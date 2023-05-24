96251b373ab09e2c670ae3ef424ff0a053a3baa3
rohan-uiuc <rohan13@illinois.edu>
2023-05-24 17:58:32 -0500

    Context cards clickable feature to open the source file in browser or download it

--- src/components/UIUC-Components/ContextCards.tsx
+++ src/components/UIUC-Components/ContextCards.tsx
@@ -16,7 +16,19 @@ import {
 import React, { useState, useEffect } from 'react'
 import { fetchContexts, getTopContextsResponse } from '~/pages/api/getContexts'
 import Link from 'next/link'
+import axios from "axios";
 
+async function fetchPresignedUrl(filePath: string) {
+  try {
+
+    const response = await axios.post('/api/download', { filePath });
+    // return response.data.url;
+    return response.data.url;
+  } catch (error) {
+    console.error('Error fetching presigned URL:', error);
+    return null;
+  }
+}
 
 export const BuildContextCards = () => {
   const [contexts, setContexts] = useState<getTopContextsResponse[]>();
@@ -53,7 +65,7 @@ export const BuildContextCards = () => {
             <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
             <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
             <div className="flex items-center mt-4 space-x-3">
-                <svg className="text-gray-200 w-14 h-14 dark:text-gray-700" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
+                <svg className="text-gray-200 w-14 h-14 dark:text-gray-700" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
                 <div>
                     <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
                     <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
@@ -75,10 +87,17 @@ function DynamicMaterialsCard({
   s3_path,
   pagenumber_or_timestamp,
 }: getTopContextsResponse) {
+  const [presignedUrl, setPresignedUrl] = useState<string | null>(null);
+
+  fetchPresignedUrl(s3_path).then((url) => {
+      setPresignedUrl(url);
+  });
+
+
   return (
     <div className="box-sizing: border-box; border: 100px solid #ccc;">
       <Link
-        href={'https://kastanday.com'}
+        href={presignedUrl || '#'}
         rel="noopener noreferrer"
         target="_blank"
       >
