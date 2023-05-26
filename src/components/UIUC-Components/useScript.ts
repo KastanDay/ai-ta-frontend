8746cc20dc925721d4e4cd62942d64ff9d7f99c4
Kastan Day <kastanvday@gmail.com>
2023-05-26 12:49:41 -0500

    huge commit. Ingest now works, just need to update the EdgeConfig with 'course exists'

---
+++ src/components/UIUC-Components/useScript.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+// for loading scripts in next.js
+// For Scite badges 
+
+const useScript = (src) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [src]);
+
+  return null;
+};
+
+export default useScript;
