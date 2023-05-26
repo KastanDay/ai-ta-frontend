6781e283c121fe98167ad0c142aa857aeafb4440
Kastan Day <kastanvday@gmail.com>
2023-05-26 13:48:42 -0500

    working on ading edgeConfig, in progress

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+
+// import { NextRequest, NextResponse } from 'next/server';
+// import { get, getAll } from '@vercel/edge-config';
+// const configItems = await getAll();
+
+// export const config = { matcher: '/*' };
+
+// export async function middleware() {
+//   const greeting = await get('greeting');
+//   return NextResponse.json(greeting);
+// }
