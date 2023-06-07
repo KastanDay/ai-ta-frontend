dce28beab22d458224ea37677a61d25a08ebfa0b
rohan-uiuc <rohan13@illinois.edu>
2023-06-07 18:55:30 -0500

    fixing size and spacing

--- src/components/UIUC-Components/MakeOldCoursePage.tsx
+++ src/components/UIUC-Components/MakeOldCoursePage.tsx
@@ -16,13 +16,13 @@ const MakeOldCoursePage = ({ course_name, course_data }: { course_name: string, 
                 <meta name="description" content="The AI teaching assistant built for students at UIUC." />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="items-center justify-left; course-page-main flex min-h-screen flex-col">
+            <main className="items-center justify-center; course-page-main flex min-h-screen flex-col">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                     <Link href="/">
                         <h2 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]"> UIUC Course <span className="${inter.style.fontFamily} text-[hsl(280,100%,70%)]">AI</span> </h2>
                     </Link>
                 </div>
-                <div className="items-center container flex flex-col justify-center gap-12 py-2 ">
+                <div className="items-left container flex flex-col justify-center gap-12 py-2">
                     <Flex direction="column" align="center" justify="center">
                         <Title className={montserrat.className} variant="gradient" gradient={{ from: 'gold', to: 'white', deg: 50 }} order={2} p="xl"> Want to upload more materials? </Title>
                         <DropzoneS3Upload course_name={course_name} />
@@ -63,17 +63,17 @@ const CourseFilesList = ({ files }: CourseFilesListProps) => {
     };
 
     return (
-        <div className="w-full mx-auto bg-violet-200 border border-violet-950 shadow-md shadow-amber-100 rounded-xl p-10">
+        <div className="w-full mx-auto justify-center bg-violet-200 border border-violet-950 shadow-md shadow-amber-100 rounded-xl p-5">
             <ul role="list" className="divide-y divide-gray-100">
                 {files.map((file) => (
-                    <li key={file.s3_path} className={`flex justify-between items-center gap-x-6 py-5 bg-violet-950 bg border border-violet-700 shadow-md shadow-amber-100 rounded-xl`}>
+                    <li key={file.s3_path} className={`flex justify-between items-center gap-x-6 py-5 bg-violet-950 bg border border-violet-700 shadow-md shadow-amber-100 rounded-2xl`}>
                         <div className="flex gap-x-4">
                             <div className="min-w-0 flex-auto">
                                 <p className="px-4 text-xl font-semibold leading-6 text-white">{file.readable_filename}</p>
                                 <p className="px-4 mt-1 truncate text-xs leading-5 text-white">{file.course_name}</p>
                             </div>
                         </div>
-                        <button onClick={() => handleDelete(file.s3_path as string)} className="bg-red-600 hover:bg-red-800 text-white w-6 h-6 flex items-center justify-center rounded-full mr-4">
+                        <button onClick={() => handleDelete(file.s3_path as string)} className="bg-red-600 hover:bg-red-800 cursor-pointer text-white w-6 h-6 flex items-center justify-center rounded-full mr-4">
                             <span className="text-xl font-bold">-</span>
                         </button>
                     </li>
