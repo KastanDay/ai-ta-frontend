b2464cc48e2a6555575d79abd70138c9693c5a5a
Rohan <rohan13@illinois.edu>
2023-06-21 17:00:12 -0500

    Changes to render collapsible model param box and render sample questions in dividers

--- src/components/Chat/ModelSelect.tsx
+++ src/components/Chat/ModelSelect.tsx
@@ -1,66 +1,87 @@
-import { IconExternalLink } from '@tabler/icons-react'
-import { useContext } from 'react'
-
-import { useTranslation } from 'next-i18next'
-
-import { OpenAIModel } from '@/types/openai'
-
-import HomeContext from '~/pages/api/home/home.context'
+import { useRef, useState, useEffect } from 'react';
+import { IconExternalLink, IconChevronDown } from '@tabler/icons-react';
+import { useContext } from 'react';
+import { useTranslation } from 'next-i18next';
+import { OpenAIModel } from '@/types/openai';
+import HomeContext from '~/pages/api/home/home.context';
 
 export const ModelSelect = () => {
-  const { t } = useTranslation('chat')
+    const { t } = useTranslation('chat');
+    const [isOpen, setIsOpen] = useState(false);
+    const wrapperRef = useRef(null);
 
-  const {
-    state: { selectedConversation, models, defaultModelId },
-    handleUpdateConversation,
-    dispatch: homeDispatch,
-  } = useContext(HomeContext)
+    const {
+        state: { selectedConversation, models, defaultModelId },
+        handleUpdateConversation,
+        dispatch: homeDispatch,
+    } = useContext(HomeContext);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    selectedConversation &&
-      handleUpdateConversation(selectedConversation, {
-        key: 'model',
-        value: models.find(
-          (model) => model.id === e.target.value,
-        ) as OpenAIModel,
-      })
-  }
+    const handleModelClick = (modelId) => {
+        setIsOpen(false);
+        selectedConversation &&
+        handleUpdateConversation(selectedConversation, {
+            key: 'model',
+            value: models.find((model) => model.id === modelId) as OpenAIModel,
+        });
+    };
 
-  return (
-    <div className="flex flex-col">
-      <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('Model')}
-      </label>
-      <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
-        <select
-          className="w-full bg-transparent p-2"
-          placeholder={t('Select a model') || ''}
-          value={selectedConversation?.model?.id || defaultModelId}
-          onChange={handleChange}
-        >
-          {models.map((model) => (
-            <option
-              key={model.id}
-              value={model.id}
-              className="dark:bg-[#343541] dark:text-white"
+    const handleClickOutside = (event) => {
+        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [wrapperRef]);
+
+    return (
+        <div className="flex flex-col">
+            <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
+                {t('Model')}
+            </label>
+            <div
+                ref={wrapperRef}
+                tabIndex={0}
+                className="relative w-full rounded-lg border border-neutral-300 bg-neutral-50 pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white"
+                onFocus={() => setIsOpen(true)}
+                onBlur={() => setIsOpen(false)}
             >
-              {model.id === defaultModelId
-                ? `Default (${model.name})`
-                : model.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mt-3 flex w-full items-center text-left text-neutral-700 dark:text-neutral-400">
-        <a
-          href="https://platform.openai.com/account/usage"
-          target="_blank"
-          className="flex items-center"
-        >
-          <IconExternalLink size={18} className={'mr-1 inline'} />
-          {t('View Account Usage')}
-        </a>
-      </div>
-    </div>
-  )
-}
+                <div className="w-full bg-transparent p-2 flex justify-between items-center">
+          <span>
+            {selectedConversation?.model?.id === defaultModelId
+                ? selectedConversation?.model?.name
+                : selectedConversation?.model?.name || 'Select a model'}
+          </span>
+                    <IconChevronDown size={18} />
+                </div>
+                {isOpen && (
+                    <ul className="absolute z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
+                        {models.map((model) => (
+                            <li key={model.id} className="dark:bg-[#343541] dark:text-white">
+                                <a onClick={() => handleModelClick(model.id)}>
+                                    {model.id === defaultModelId
+                                        ? model.name
+                                        : model.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+            <div className="mt-3 flex w-full items-center text-left text-neutral-700 dark:text-neutral-400">
+                <a
+                    href="https://platform.openai.com/account/usage"
+                    target="_blank"
+                    className="flex items-center"
+                >
+                    <IconExternalLink size={18} className={'mr-1 inline'} />
+                    {t('View Account Usage')}
+                </a>
+            </div>
+        </div>
+    );
+};
