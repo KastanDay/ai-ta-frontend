acd7fc9ebafe8a352d81b18e819c63541f99b99b
Kastan Day <kastanvday@gmail.com>
2023-07-05 18:27:32 -0700

    prompt stuffing: fixing the stuffed prompt formatting

--- src/pages/api/getExtremePrompt.ts
+++ src/pages/api/getExtremePrompt.ts
@@ -5,7 +5,7 @@ export const config = {
 export async function getExtremePrompt(
   course_name: string,
   search_query: string,
-  top_n = 8, // get 20 contexts. Batch size is 20, so this is the minimum & max for a single call.
+  top_n = 15, // get 20 contexts. Batch size is 20, so this is the minimum & max for a single call.
   top_k_to_search = 100,
 ) {
   console.log('getExtremePrompt called')
@@ -20,7 +20,8 @@ export async function getExtremePrompt(
     throw new Error('Failed to fetch contexts. Err status:' + res.status)
   }
 
-  const extremePrompt: string = await res.json()
-  console.log('ExtremePrompt:\n', extremePrompt)
-  return extremePrompt
+  const extremePrompt = await res.json()
+  const finalPromptStr: string = extremePrompt.prompt
+  // console.log('finalPromptStr:\n', finalPromptStr)
+  return finalPromptStr
 }
