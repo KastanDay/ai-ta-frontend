25f3256514fef23ba4aee50a98e201ae0bd0958f
Kastan Day <kastanvday@gmail.com>
2023-07-07 14:34:22 -0700

    Design refresh: context cards are smaller and don't contain broken images

--- src/pages/privacystatement.tsx
+++ src/pages/privacystatement.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next"
+import { NextPage } from 'next'
 
 const PrivacyStatement: NextPage = (props) => {
   return (
--- src/pages/termsofservice.tsx
+++ src/pages/termsofservice.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next"
+import { NextPage } from 'next'
 
 const TermsOfService: NextPage = (props) => {
   return (
