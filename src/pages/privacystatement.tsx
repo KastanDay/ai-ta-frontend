995313f26ea5c7c1970800a6c73cea390e497070
Kastan Day <kastanvday@gmail.com>
2023-07-07 11:46:33 -0700

    adding terms and privacy statemetns

---
+++ src/pages/privacystatement.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+
+const PrivacyStatement: NextPage = (props) => {
+  return (
+    <div>
+      <h1>Privacy Statement</h1>
+      <p>This is the privacy statement.</p>
+    </div>
+  )
+}
+
+export default PrivacyStatement
