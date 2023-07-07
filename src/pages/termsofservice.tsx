6cc00b28532e7d86c61ecfa36c6cfe003d72fbe4
Kastan Day <kastanvday@gmail.com>
2023-07-07 11:46:33 -0700

    adding terms and privacy statemetns

---
+++ src/pages/termsofservice.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+
+const TermsOfService: NextPage = (props) => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>These are the terms of service.</p>
+    </div>
+  )
+}
+
+export default TermsOfService
