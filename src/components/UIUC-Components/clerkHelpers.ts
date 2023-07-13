1a24678f4ccc705fc7f296247ab357904d5e9709
Kastan Day <kastanvday@gmail.com>
2023-07-13 13:46:08 -0700

    HOTFIX course_owner was becoming 'undefined', not able to view own course

--- src/components/UIUC-Components/clerkHelpers.ts
+++ src/components/UIUC-Components/clerkHelpers.ts
@@ -1,16 +1,18 @@
 import { UserResource } from '@clerk/types'
 
 export function extractEmailsFromClerk(
-  user: UserResource | undefined | null,
+  clerk_user: UserResource | undefined | null,
 ): string[] {
   // Extract all possible emails from Clerk user object.
   // Both manually set emails and social logins.
-  if (!user) return []
+  if (!clerk_user) return []
 
-  const externalEmails =
-    user.externalAccounts?.map((account) => account.emailAddress) || []
-  const userEmails =
-    user.emailAddresses?.map((account) => account.emailAddress) || []
+  const externalEmails = clerk_user.externalAccounts?.map(
+    (account) => account.emailAddress as string,
+  ) as string[]
+  const userEmails = clerk_user.emailAddresses?.map(
+    (account) => account.emailAddress as string,
+  ) as string[]
 
   const emails = [...externalEmails, ...userEmails].filter(
     (email) => email !== undefined,
