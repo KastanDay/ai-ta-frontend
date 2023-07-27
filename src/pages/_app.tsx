74c6e3bd1bb6f73ae71ffc16a0578a016b87f7af
Kastan Day <kastanvday@gmail.com>
2023-07-26 17:36:55 -0700

    HOT CRASH -- TAKING US 100% DOWN FOR MAINTENCE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,62 +10,69 @@ import { dark } from '@clerk/themes'
 import { api } from '~/utils/api'
 
 import '~/styles/globals.css'
+import Maintenance from '~/components/UIUC-Components/Maintenance'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-      {...pageProps}
-    >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: 'dark',
-          colors: {
-            // Add your color
-            deepBlue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-            lime: ['#a3e635', '#65a30d', '#365314' /* ... */],
-            aiPurple: ['#C06BF9'],
-            backgroundColors: ['#2e026d', '#020307'],
-            nearlyBlack: ['#0E1116'],
-            nearlyWhite: ['#F7F7F7'],
-            disabled: ['#2A2F36'],
-            // or replace default theme color
-            // blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-          },
-          // primaryColor: 'aiPurple',
-
-          shadows: {
-            // md: '1px 1px 3px rgba(0, 0, 0, .25)',
-            // xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-          },
-
-          headings: {
-            fontFamily: 'Montserrat, Roboto, sans-serif',
-            sizes: {
-              h1: { fontSize: '3rem' },
-              h2: { fontSize: '2.2rem' },
-            },
-          },
-          defaultGradient: {
-            from: '#dc2626',
-            to: '#431407',
-            deg: 80,
-          },
+  if (process.env.NEXT_PUBLIC_MAINTENANCE === 'true') {
+    return <Maintenance />;
+  }
+  else {
+    return (
+      <ClerkProvider
+        appearance={{
+          baseTheme: dark,
         }}
+        {...pageProps}
       >
-        <Notifications position="bottom-center" zIndex={2077} />
-        <Component {...pageProps} />
-        <Analytics />
-      </MantineProvider>
-    </ClerkProvider>
-  )
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: 'dark',
+            colors: {
+              // Add your color
+              deepBlue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+              lime: ['#a3e635', '#65a30d', '#365314' /* ... */],
+              aiPurple: ['#C06BF9'],
+              backgroundColors: ['#2e026d', '#020307'],
+              nearlyBlack: ['#0E1116'],
+              nearlyWhite: ['#F7F7F7'],
+              disabled: ['#2A2F36'],
+              // or replace default theme color
+              // blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+            },
+            // primaryColor: 'aiPurple',
+  
+            shadows: {
+              // md: '1px 1px 3px rgba(0, 0, 0, .25)',
+              // xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+            },
+  
+            headings: {
+              fontFamily: 'Montserrat, Roboto, sans-serif',
+              sizes: {
+                h1: { fontSize: '3rem' },
+                h2: { fontSize: '2.2rem' },
+              },
+            },
+            defaultGradient: {
+              from: '#dc2626',
+              to: '#431407',
+              deg: 80,
+            },
+          }}
+        >
+          <Notifications position="bottom-center" zIndex={2077} />
+          <Component {...pageProps} />
+          <Analytics />
+        </MantineProvider>
+      </ClerkProvider>
+    )
+  }
+  
 }
 
 // export default .withTRPC(MyApp)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,24 @@ type Props = DocumentProps & {
 export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale
+  
+  if (process.env.NEXT_PUBLIC_MAINTENANCE === 'true') {
+    return (
+      <Html lang={currentLocale}>
+        <Head>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+          {/* Prevent search engine indexing of Maintenance page: https://github.com/vercel/next.js/discussions/12850#discussioncomment-3335807  */}
+          <meta name="robots" content="noindex" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
   return (
     <Html lang={currentLocale}>
       <Head>
