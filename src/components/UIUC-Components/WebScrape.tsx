5ed80182edc4f7429179f2f990c8db3ad5fb6b60
rohanmarwaha <rohan13@illinois.edu>
2023-08-21 19:46:30 -0500

    Fixing upsert integration in Webscrape

--- src/components/UIUC-Components/WebScrape.tsx
+++ src/components/UIUC-Components/WebScrape.tsx
@@ -8,7 +8,6 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import { useMediaQuery } from '@mantine/hooks'
 import { LoadingSpinner } from '~/components/UIUC-Components/LoadingSpinner'
-import { callUpsertCourseMetadata } from '~/pages/api/UIUC-api/upsertCourseMetadata'
 import { setCourseExistsAPI } from './Upload_S3'
 
 interface WebScrapeProps {
@@ -107,15 +106,28 @@ export const WebScrape = ({
           // await setCourseExistsAPI(courseName) 
 
           // set course exists in new metadata endpoint. Works great.
-          await callUpsertCourseMetadata(courseName, {
-            course_owner: current_user_email,
-            // Don't set properties we don't know about. We'll just upsert and use the defaults.
-            course_admins: undefined,
-            approved_emails_list: undefined,
-            is_private: undefined,
-            banner_image_s3: undefined,
-            course_intro_message: undefined,
+          const response = await fetch('/api/UIUC-api/upsertCourseMetadata', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              courseName: courseName,
+              courseMetadata: {
+                course_owner: current_user_email,
+                // Don't set properties we don't know about. We'll just upsert and use the defaults.
+                course_admins: undefined,
+                approved_emails_list: undefined,
+                is_private: undefined,
+                banner_image_s3: undefined,
+                course_intro_message: undefined,
+              },
+            }),
           })
+          const data = await response.json();
+          if (!data.success) {
+            throw new Error(data.error);
+          }
           router.replace(`/${courseName}/materials`)
         }
         router.push(`/${courseName}/materials`)
