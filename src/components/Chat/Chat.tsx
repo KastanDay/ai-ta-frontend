2410e9e0da9a71312744b777e275ef14f9374682
Kastan Day <kastanvday@gmail.com>
2023-08-24 13:05:58 -0700

    add chat settings header to new chat page (because it has important links to upload and disclaimer

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -541,9 +541,70 @@ export const Chat = memo(({ stopConversationRef, courseMetadata }: Props) => {
             ref={chatContainerRef}
             onScroll={handleScroll}
           >
+            {/* Always render the 'model, upload, disclaimer' banner */}
+            <div className="sticky top-0 z-10 flex w-full flex-col justify-center bg-neutral-100 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
+              <div className="flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
+                <button
+                  className="ml-2 cursor-pointer hover:opacity-50"
+                  onClick={handleSettings}
+                >
+                  <div className="flex items-center">
+                    {t('Model')}: {selectedConversation?.model.name}
+                    <span className="w-2" />
+                    <IconRobot size={18} />
+                  </div>
+                </button>
+                <span className="w-3" />
+                |
+                <span className="w-3" />
+                <button
+                  className="ml-2 cursor-pointer hover:opacity-50"
+                  onClick={redirectToMaterialsPage}
+                >
+                  <div className="flex items-center">
+                    <Text
+                      variant="gradient"
+                      weight={600}
+                      gradient={{ from: 'gold', to: 'white', deg: 50 }}
+                    >
+                      Upload materials
+                    </Text>
+                    &nbsp;&nbsp;
+                    <IconCloudUpload size={18} />
+                  </div>
+                </button>
+                &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;
+                <a
+                  className="ml-2 cursor-pointer hover:opacity-50"
+                  href="/disclaimer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center">
+                    <span>
+                      <Text
+                        variant="gradient"
+                        weight={400}
+                        gradient={{ from: 'white', to: 'white', deg: 50 }}
+                      >
+                        Disclaimer: it&apos;s not perfect
+                      </Text>
+                    </span>
+                    &nbsp;&nbsp;
+                    <IconExternalLink size={18} />
+                  </div>
+                </a>
+              </div>
+            </div>
+            {showSettings && (
+              <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+                <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
+                  <ModelSelect />
+                </div>
+              </div>
+            )}
             {selectedConversation?.messages.length === 0 ? (
               <>
-                {/* <CustomBanner bannerUrl={bannerUrl as string} /> Banner on fresh chat page */}
                 {bannerUrl && (
                   <div style={{ width: '100%' }}>
                     <img
@@ -579,82 +640,7 @@ export const Chat = memo(({ stopConversationRef, courseMetadata }: Props) => {
               </>
             ) : (
               <>
-                <div className="sticky top-0 z-10 flex w-full flex-col justify-center bg-neutral-100 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                  <div className="flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                    {/* Clear current conversation. */}
-                    {/* <button
-                      className="ml-2 cursor-pointer hover:opacity-50"
-                      onClick={onClearAll}
-                    >
-                      <IconClearAll size={18} />
-                    </button>
-                    &nbsp;&nbsp;|&nbsp;&nbsp; */}
-                    {/* {t('Temp')}: {selectedConversation?.temperature}*/}
-                    {/* BUTTONS Change Model */}
-                    <button
-                      className="ml-2 cursor-pointer hover:opacity-50"
-                      onClick={handleSettings}
-                    >
-                      <div className="flex items-center">
-                        {t('Model')}: {selectedConversation?.model.name}
-                        <span className="w-2" />
-                        <IconRobot size={18} />
-                      </div>
-                    </button>
-                    <span className="w-3" />
-                    |
-                    <span className="w-3" />
-                    <button
-                      className="ml-2 cursor-pointer hover:opacity-50"
-                      onClick={redirectToMaterialsPage}
-                    >
-                      <div className="flex items-center">
-                        <Text
-                          variant="gradient"
-                          weight={600}
-                          gradient={{ from: 'gold', to: 'white', deg: 50 }}
-                          // className={"goldUnderline"}
-                        >
-                          Upload materials
-                        </Text>
-                        &nbsp;&nbsp;
-                        <IconCloudUpload size={18} />
-                      </div>
-                    </button>
-                    &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;
-                    {/* Disclaimer: it's not perfect (a tag to open in new tab) */}
-                    <a
-                      className="ml-2 cursor-pointer hover:opacity-50"
-                      href="/disclaimer"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {/* Disclaimer */}
-                      <div className="flex items-center">
-                        <span>
-                          <Text
-                            variant="gradient"
-                            weight={400}
-                            gradient={{ from: 'white', to: 'white', deg: 50 }}
-                          >
-                            Disclaimer: it&apos;s not perfect
-                          </Text>
-                        </span>
-                        &nbsp;&nbsp;
-                        <IconExternalLink size={18} />
-                      </div>
-                    </a>
-                  </div>
-                </div>
-                {showSettings && (
-                  <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-                    <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
-                      <ModelSelect />
-                    </div>
-                  </div>
-                )}
                 <CustomBanner bannerUrl={bannerUrl as string} />{' '}
-                {/* Banner on "chat with messages" page (not fresh chat) */}
                 {selectedConversation?.messages.map((message, index) => (
                   <MemoizedChatMessage
                     key={index}
@@ -662,7 +648,6 @@ export const Chat = memo(({ stopConversationRef, courseMetadata }: Props) => {
                     messageIndex={index}
                     onEdit={(editedMessage) => {
                       setCurrentMessage(editedMessage)
-                      // discard edited message and the ones that come after then resend
                       handleSend(
                         editedMessage,
                         selectedConversation?.messages.length - index,
