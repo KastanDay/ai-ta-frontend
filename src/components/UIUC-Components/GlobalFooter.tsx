23628071f36c1dc70b6aecd0a03d54b418633144
Maxwell-Lindsey <90484034+Maxwell-Lindsey@users.noreply.github.com>
2023-08-28 16:02:24 -0500

    add new GlobalFooter.tsx for disclaimer (#28)
    * test commit
    
    * Added GlobalFooter
    
    * remove duplicate imports
    
    * npm run format:fix ONLY
    
    * npm run format:fix PLUS removal from MainPageBackground and spinners
    
    * fix imports, my bad bro
    
    ---------
    
    Co-authored-by: Kastan Day <kastanvday@gmail.com>

---
+++ src/components/UIUC-Components/GlobalFooter.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link' // Import the Link component
+
+export default function Footer({ isNavbar = false }: { isNavbar?: boolean }) {
+  return (
+    <footer className="footer footer-center rounded p-10 text-base-content">
+      <div className="grid grid-flow-col gap-4">
+        <Link href="/disclaimer" className="link-hover link text-blue-500">
+          Disclaimer
+        </Link>
+        <Link href="/terms" className="link-hover link text-blue-500">
+          Terms
+        </Link>
+        <Link href="/privacy" className="link-hover link text-blue-500">
+          Privacy
+        </Link>
+        <Link
+          href="https://github.com/kastanday/ai-ta-frontend"
+          className="link-hover link text-blue-500"
+        >
+          MIT Licensed
+        </Link>
+      </div>
+    </footer>
+  )
+}
