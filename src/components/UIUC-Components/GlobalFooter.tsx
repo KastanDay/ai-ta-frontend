1e816e3309b4557e3f3d2dac817eec9f2679fe94
Kastan Day <kastanvday@gmail.com>
2023-08-28 15:40:19 -0700

    footer enhancement: open links in new tab, add frontend AND backend code links (cuz everyone asks)

--- src/components/UIUC-Components/GlobalFooter.tsx
+++ src/components/UIUC-Components/GlobalFooter.tsx
@@ -4,21 +4,51 @@ export default function Footer({ isNavbar = false }: { isNavbar?: boolean }) {
   return (
     <footer className="footer footer-center rounded p-10 text-base-content">
       <div className="grid grid-flow-col gap-4">
-        <Link href="/disclaimer" className="link-hover link text-blue-500">
+        <Link
+          href="/disclaimer"
+          className="link-hover link text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Disclaimer
         </Link>
-        <Link href="/terms" className="link-hover link text-blue-500">
+        <Link
+          href="/terms"
+          className="link-hover link text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Terms
         </Link>
-        <Link href="/privacy" className="link-hover link text-blue-500">
+        <Link
+          href="/privacy"
+          className="link-hover link text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Privacy
         </Link>
-        <Link
-          href="https://github.com/kastanday/ai-ta-frontend"
-          className="link-hover link text-blue-500"
-        >
-          MIT Licensed
-        </Link>
+        <span>
+          MIT Licensed{' '}
+          <Link
+            href="https://github.com/kastanday/ai-ta-frontend"
+            className="link-hover link text-blue-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            frontend
+          </Link>{' '}
+          and{' '}
+          <Link
+            href="https://github.com/UIUC-Chatbot/ai-ta-backend"
+            className="link-hover link text-blue-500"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            backend
+          </Link>
+          {' '}code.
+        </span>
       </div>
     </footer>
   )
