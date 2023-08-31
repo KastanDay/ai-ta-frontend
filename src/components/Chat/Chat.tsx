04141cd09505765a61a4ef4e1eedc4939dc36f87
Kastan Day <kastanvday@gmail.com>
2023-08-30 17:14:06 -0700

    minor improvement to API key message

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -22,7 +22,7 @@ import {
   useState,
 } from 'react'
 import toast from 'react-hot-toast'
-import { Text } from '@mantine/core'
+import { Text, Title } from '@mantine/core'
 import { useTranslation } from 'next-i18next'
 
 import { getEndpoint } from '@/utils/app/api'
@@ -470,11 +470,11 @@ export const Chat = memo(({ stopConversationRef, courseMetadata }: Props) => {
   const statements = courseMetadata?.course_intro_message
     ? courseMetadata.course_intro_message.split('\n')
     : [
-        'Make a bullet point list of key takeaways of the course.',
-        'What is [your favorite topic] and why is it worth learning about?',
-        'How can I effectively prepare for the upcoming exam?',
-        'How many assignments in the course?',
-      ]
+      'Make a bullet point list of key takeaways of the course.',
+      'What is [your favorite topic] and why is it worth learning about?',
+      'How can I effectively prepare for the upcoming exam?',
+      'How many assignments in the course?',
+    ]
 
   // Add this function to create dividers with statements
   const renderDividers = () => {
@@ -497,38 +497,37 @@ export const Chat = memo(({ stopConversationRef, courseMetadata }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            UIUC Course AI
           </div>
-          <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
-            </div>
-          </div>
+          <div className="text-center text-lg text-black dark:text-white"></div>
           <div className="text-center text-gray-500 dark:text-gray-400">
-            <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
-            </div>
-            <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
-            </div>
-            <div className="mb-2">
-              {t(
-                'Please set your OpenAI API key in the bottom left of the sidebar.',
-              )}
-            </div>
-            <div>
-              {t("If you don't have an OpenAI API key, you can get one here: ")}
-              <a
-                href="https://platform.openai.com/account/api-keys"
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                openai.com
-              </a>
+            <div className="rounded border border-2 border-solid border-red-500 p-4">
+              <Title>⚠️</Title>
+              <div className="mb-2">
+                <Title
+                  // className={montserrat.className}
+                  variant="gradient"
+                  gradient={{ from: 'red', to: 'white', deg: 50 }}
+                  order={3}
+                  p="xl"
+                >
+                  Please set your OpenAI API key in the bottom left of the
+                  sidebar.
+                </Title>
+              </div>
+              <div>
+                <Text size={'md'}>
+                  If you don&apos;t have an OpenAI API key, you can get one here:{' '}
+                  <a
+                    href="https://platform.openai.com/account/api-keys"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    openai.com
+                  </a>
+                </Text>
+              </div>
             </div>
           </div>
         </div>
