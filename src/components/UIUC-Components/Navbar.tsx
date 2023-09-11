e82c93d33f400ad148ab1ba1d98df858e5c1bf8e
Ruixin Han <hanruixin1999@163.com>
2023-09-11 16:29:50 -0500

    navigational bar

--- src/components/UIUC-Components/Navbar.tsx
+++ src/components/UIUC-Components/Navbar.tsx
@@ -2,26 +2,92 @@ import Link from 'next/link'
 import GlobalHeader from '~/components/UIUC-Components/GlobalHeader'
 import { Flex } from '@mantine/core'
 import ResumeToChat from './ResumeToChat'
+import { useState } from 'react';
+import { createStyles, Header, Container, Anchor, Group, Burger, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-const Navbar = ({ course_name = '' }) => (
-  <div className="flex flex-col items-center bg-[#2e026d]">
-    <div className="mt-4 w-full max-w-[95%]">
-      <div className="navbar rounded-badge h-24 min-h-fit bg-[#15162c] shadow-lg shadow-purple-800">
-        <div className="flex-1">
-          <Link href="/">
-            <h2 className="ms-8 cursor-pointer text-3xl font-extrabold tracking-tight text-white sm:text-[2rem] ">
-              UIUC Course <span className="text-[hsl(280,100%,70%)]">AI</span>
-            </h2>
-          </Link>
-        </div>
-        <Flex direction="row" align="center" justify="center">
-          <div className="ms-4 mt-4 flex flex-row items-center justify-center gap-2">
-            <ResumeToChat course_name={course_name} />
+
+
+// a navbar the is inspired by github horizontal bar
+const HEADER_HEIGHT = rem(84);
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    height: HEADER_HEIGHT,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  links: {
+    paddingTop: theme.spacing.lg,
+    height: HEADER_HEIGHT,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    textTransform: 'uppercase',
+    fontSize: rem(13),
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[6],
+    padding: `${rem(7)} ${theme.spacing.sm}`,
+    fontWeight: 700,
+    borderBottom: `${rem(2)} solid transparent`,
+    transition: 'border-color 100ms ease, color 100ms ease',
+
+    '&:hover': {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      textDecoration: 'none',
+    },
+  },
+}));
+
+const Navbar = ({ course_name = '' }) => {
+  const [isOpen, setOpen] = useState(false);
+  const { classes, cx } = useStyles();
+
+  return (
+    <div className="flex flex-col items-center bg-[#2e026d]">
+      <div className="mt-4 w-full max-w-[95%]">
+        <div className="navbar rounded-badge h-24 min-h-fit bg-[#15162c] shadow-lg shadow-purple-800">
+          <div className="flex-1">
+            <Link href="/">
+              <h2 className="ms-8 cursor-pointer text-3xl font-extrabold tracking-tight text-white sm:text-[2rem] ">
+                UIUC Course <span className="text-[hsl(280,100%,70%)]">AI</span>
+              </h2>
+            </Link>
           </div>
-        </Flex>
 
-        {/* THIS BUTTON IS FOR "GPT4" CHAT */}
-        {/* <button
+          <Header height={HEADER_HEIGHT} mb={120}>
+            <Container className={classes.inner}>
+              <div className={classes.links}>
+                <Link href="/chat" className={classes.link}>Chat</Link>
+                <Link href="/materials" className={classes.link}>Materials</Link>
+                <Link href="/analysis" className={classes.link}>Analysis</Link>
+                <Link href="/setting" className={classes.link}>Setting</Link>
+              </div>
+            </Container>
+          </Header>
+
+          <Flex direction="row" align="center" justify="center">
+            <div className="ms-4 mt-4 flex flex-row items-center justify-center gap-2">
+              <ResumeToChat course_name={course_name} />
+            </div>
+          </Flex>
+
+          {/* THIS BUTTON IS FOR "GPT4" CHAT */}
+          {/* <button
               className={`btn-circle btn mb-1 ms-4`}
               style={{
                 position: 'relative',
@@ -101,7 +167,7 @@ const Navbar = ({ course_name = '' }) => (
                 GPT4
               </span>{' '}
               {/* Adjusted the vertical position of the text */}
-        {/* <svg
+          {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
@@ -121,10 +187,27 @@ const Navbar = ({ course_name = '' }) => (
               </svg>
             </button> */}
 
-        <GlobalHeader isNavbar={true} />
+          <GlobalHeader isNavbar={true} />
+        </div>
       </div>
     </div>
-  </div>
-)
+  );
+}
 
+// export function Navbar() {
+//   const { classes, cx } = useStyles();
+
+//   return (
+//     <Header height={HEADER_HEIGHT} mb={120}>
+//       <Container className={classes.inner}>
+//         <div className={classes.links}>
+//           <Link href="/chat" className={classes.link}>Chat</Link>
+//           <Link href="/materials" className={classes.link}>Materials</Link>
+//           <Link href="/analysis" className={classes.link}>Analysis</Link>
+//           <Link href="/setting" className={classes.link}>Setting</Link>
+//         </div>
+//       </Container>
+//     </Header>
+//   );
+// }
 export default Navbar
