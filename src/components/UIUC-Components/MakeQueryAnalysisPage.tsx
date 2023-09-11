58274ad640a3b30baec80fc73370ecc062b016c5
Kastan Day <kastanvday@gmail.com>
2023-09-11 12:22:07 -0700

    move all navbar buttons to single file, cleanup duplicated code

--- src/components/UIUC-Components/MakeQueryAnalysisPage.tsx
+++ src/components/UIUC-Components/MakeQueryAnalysisPage.tsx
@@ -129,7 +129,7 @@ const MakeQueryAnalysisPage = ({
     return (
       <CannotEditCourse
         course_name={currentPageName as string}
-        // current_email={currentEmail as string}
+      // current_email={currentEmail as string}
       />
     )
   }
@@ -270,7 +270,6 @@ import { MainPageBackground } from './MainPageBackground'
 import { LoadingSpinner } from './LoadingSpinner'
 import { extractEmailsFromClerk } from './clerkHelpers'
 import QueryAnalysisNavbar from '~/components/UIUC-Components/QueryAnalysisNavbar'
-import EditCourseCard from '~/components/UIUC-Components/EditCourseCard'
 import { notifications } from '@mantine/notifications'
 import GlobalFooter from './GlobalFooter'
 
@@ -364,15 +363,15 @@ const CourseFilesList = ({ files }: CourseFilesListProps) => {
                 // style={{ outline: 'solid 1px', outlineColor: 'white' }}
                 onMouseEnter={(e) => {
                   e.currentTarget.style.backgroundColor = theme.colors.grape[8]
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colorScheme === 'dark'
-                      ? theme.colors.gray[2]
-                      : theme.colors.gray[1]
+                    ; (e.currentTarget.children[0] as HTMLElement).style.color =
+                      theme.colorScheme === 'dark'
+                        ? theme.colors.gray[2]
+                        : theme.colors.gray[1]
                 }}
                 onMouseLeave={(e) => {
                   e.currentTarget.style.backgroundColor = 'transparent'
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colors.gray[8]
+                    ; (e.currentTarget.children[0] as HTMLElement).style.color =
+                      theme.colors.gray[8]
                 }}
               >
                 <IconDownload className="h-5 w-5 text-gray-800" />
@@ -389,15 +388,15 @@ const CourseFilesList = ({ files }: CourseFilesListProps) => {
                 // style={{ outline: 'solid 1px', outlineColor: theme.white }}
                 onMouseEnter={(e) => {
                   e.currentTarget.style.backgroundColor = theme.colors.grape[8]
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colorScheme === 'dark'
-                      ? theme.colors.gray[2]
-                      : theme.colors.gray[1]
+                    ; (e.currentTarget.children[0] as HTMLElement).style.color =
+                      theme.colorScheme === 'dark'
+                        ? theme.colors.gray[2]
+                        : theme.colors.gray[1]
                 }}
                 onMouseLeave={(e) => {
                   e.currentTarget.style.backgroundColor = 'transparent'
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colors.red[6]
+                    ; (e.currentTarget.children[0] as HTMLElement).style.color =
+                      theme.colors.red[6]
                 }}
               >
                 <IconTrash className="h-5 w-5 text-red-600" />
@@ -434,8 +433,7 @@ async function fetchCourseMetadata(course_name: string) {
       return data.course_metadata
     } else {
       throw new Error(
-        `Error fetching course metadata: ${
-          response.statusText || response.status
+        `Error fetching course metadata: ${response.statusText || response.status
         }`,
       )
     }
---
+++ src/components/UIUC-Components/NavbarButtons.tsx
@@ -0,0 +1,231 @@
+import { useRouter } from 'next/router'
+import { createStyles, Group, Text } from '@mantine/core'
+import React, { useState } from 'react'
+import { IconFolders, IconReportAnalytics } from '@tabler/icons-react'
+import { montserrat_paragraph } from 'fonts'
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    marginBottom: theme.spacing.md,
+    maxWidth: '340px',
+    width: '100%',
+  },
+
+  button: {
+    width: '100%',
+    border: 'none',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: theme.spacing.xs,
+    borderRadius: theme.radius.md,
+    cursor: 'pointer',
+    transition: 'background-color 0.2s ease-in-out',
+    '--btn-text-case': 'none',
+    height: '48px',
+  },
+}))
+
+export function GoToQueryAnalysis({ course_name }: { course_name?: string }) {
+  const router = useRouter()
+  const { classes, theme } = useStyles()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = (e: React.MouseEvent) => {
+    // If ctrl/cmd key is not pressed, prevent default behavior and programmatically navigate
+    if (!e.ctrlKey && !e.metaKey) {
+      e.preventDefault()
+      setIsLoading(true)
+      router.push(`/${course_name}/query-analysis`)
+    }
+    // If ctrl/cmd key is pressed, do nothing and let the browser handle the new tab opening
+  }
+
+  if (!course_name) {
+    return null
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <a
+        href={`/${course_name}/query-analysis`}
+        onClick={handleClick}
+        className={`btn rounded-full ${classes.button}`}
+        style={{
+          backgroundColor: 'transparent',
+          outline: `solid 1.5px ${theme.colors.grape[8]}`,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          minWidth: '100%', // Add this line to ensure the button maintains its width
+        }}
+        onMouseEnter={(e) =>
+          (e.currentTarget.style.backgroundColor = theme.colors.grape[8])
+        }
+        onMouseLeave={(e) =>
+          (e.currentTarget.style.backgroundColor = 'transparent')
+        }
+      >
+        <div style={{ visibility: isLoading ? 'hidden' : 'visible' }}>
+          <Group style={{ gap: '5px' }}>
+            <Text
+              size={theme.fontSizes.sm}
+              className={`${montserrat_paragraph.variable} font-montserratParagraph`}
+              color={
+                theme.colorScheme === 'dark'
+                  ? theme.colors.gray[0]
+                  : theme.black
+              }
+            >
+              Query Analysis
+            </Text>
+            <IconReportAnalytics color="white" />
+          </Group>
+        </div>
+        {isLoading && (
+          <div style={{ position: 'absolute' }}>
+            <span className="loading loading-spinner loading-xs"></span>
+          </div>
+        )}
+      </a>
+    </div>
+  )
+}
+
+export function ResumeToChat({ course_name }: { course_name?: string }) {
+  const router = useRouter()
+  const { classes, theme } = useStyles()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = (e: React.MouseEvent) => {
+    // If ctrl/cmd key is not pressed, prevent default behavior and programmatically navigate
+    if (!e.ctrlKey && !e.metaKey) {
+      e.preventDefault()
+      setIsLoading(true)
+      router.push(`/${course_name}/query-analysis`)
+    }
+    // If ctrl/cmd key is pressed, do nothing and let the browser handle the new tab opening
+  }
+
+  if (!course_name) {
+    return null
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <button
+        onClick={handleClick}
+        className={`btn rounded-full ${classes.button}`}
+        style={{
+          backgroundColor: 'transparent',
+          outline: `solid 1.5px ${theme.colors.grape[8]}`,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          minWidth: '100%',
+        }}
+        onMouseEnter={(e) =>
+          (e.currentTarget.style.backgroundColor = theme.colors.grape[8])
+        }
+        onMouseLeave={(e) =>
+          (e.currentTarget.style.backgroundColor = 'transparent')
+        }
+      >
+        <div style={{ visibility: isLoading ? 'hidden' : 'visible' }}>
+          <Group style={{ gap: '5px' }}>
+            <Text
+              size={theme.fontSizes.sm}
+              className={`${montserrat_paragraph.variable} font-montserratParagraph`}
+              color={
+                theme.colorScheme === 'dark'
+                  ? theme.colors.gray[0]
+                  : theme.black
+              }
+            >
+              Back to Chat with Documents
+            </Text>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 11h.01M8 11h.01M16 11h.01m-3.72 6.998C18.096 17.934 21 15.918 21 11c0-5-3-7-9-7s-9 2-9 7c0 3.077 1.136 5.018 3.409 6.056L5 21l7.29-3.002Z"
+                stroke="#FFF"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Group>
+        </div>
+        {isLoading && (
+          <div style={{ position: 'absolute' }}>
+            <span className="loading loading-spinner loading-xs"></span>
+          </div>
+        )}
+      </button>
+    </div>
+  )
+}
+
+export function GoToMaterials({ course_name }: { course_name?: string }) {
+  const router = useRouter()
+  const { classes, theme } = useStyles()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = () => {
+    setIsLoading(true)
+    router.push(`/${course_name}/materials`)
+  }
+
+  if (!course_name) {
+    return null
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <button
+        onClick={handleClick}
+        className={`btn rounded-full ${classes.button}`}
+        style={{
+          backgroundColor: 'transparent',
+          outline: `solid 1.5px ${theme.colors.grape[8]}`,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          minWidth: '100%',
+        }}
+        onMouseEnter={(e) =>
+          (e.currentTarget.style.backgroundColor = theme.colors.grape[8])
+        }
+        onMouseLeave={(e) =>
+          (e.currentTarget.style.backgroundColor = 'transparent')
+        }
+      >
+        <div style={{ visibility: isLoading ? 'hidden' : 'visible' }}>
+          <Group style={{ gap: '5px' }}>
+            <Text
+              size={theme.fontSizes.sm}
+              className={`${montserrat_paragraph.variable} font-montserratParagraph`}
+              color={
+                theme.colorScheme === 'dark'
+                  ? theme.colors.gray[0]
+                  : theme.black
+              }
+            >
+              Go to Materials
+            </Text>
+            <IconFolders color="white" />
+          </Group>
+        </div>
+        {isLoading && (
+          <div style={{ position: 'absolute' }}>
+            <span className="loading loading-spinner loading-xs"></span>
+          </div>
+        )}
+      </button>
+    </div>
+  )
+}
