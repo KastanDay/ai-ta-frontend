88f7be9bc83d74c6c828d0e91ee57cc59c80ac4c
Kastan Day <kastanvday@gmail.com>
2023-09-14 11:27:12 -0700

    HOTFIX: force localStorage into dark mode for all users

--- src/utils/app/settings.ts
+++ src/utils/app/settings.ts
@@ -4,13 +4,14 @@ const STORAGE_KEY = 'settings'
 
 export const getSettings = (): Settings => {
   let settings: Settings = {
-    theme: 'dark',
+    theme: 'dark', // Force dark mode for all users
     cooldown: 0,
   }
   const settingsJson = localStorage.getItem(STORAGE_KEY)
   if (settingsJson) {
     try {
       const savedSettings = JSON.parse(settingsJson) as Settings
+      savedSettings.theme = 'dark'; // Override any saved theme settings to force dark mode
       settings = Object.assign(settings, savedSettings)
     } catch (e) {
       console.error(e)
@@ -20,5 +21,6 @@ export const getSettings = (): Settings => {
 }
 
 export const saveSettings = (settings: Settings) => {
+  settings.theme = 'dark'; // Ensure dark mode is saved for all users
   localStorage.setItem(STORAGE_KEY, JSON.stringify(settings))
 }
