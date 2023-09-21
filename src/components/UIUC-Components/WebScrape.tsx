92090e5b43c3d3167adfea0623d3f9a8b06c574a
Maxwell-Lindsey <mal13@illinois.edu>
2023-09-21 13:32:25 -0500

    moved all web scrape ui code into WebScrape.tsx

--- src/components/UIUC-Components/WebScrape.tsx
+++ src/components/UIUC-Components/WebScrape.tsx
@@ -1,5 +1,5 @@
 import { notifications } from '@mantine/notifications'
-import { rem, Button, Input, Title, Text, useMantineTheme } from '@mantine/core'
+import { rem, Button, Input, Title, Text, useMantineTheme, Tooltip, Checkbox, TextInput} from '@mantine/core'
 import { IconWorldDownload } from '@tabler/icons-react'
 import React, { useEffect, useState } from 'react'
 import { Montserrat } from 'next/font/google'
@@ -8,7 +8,6 @@ import { useRouter } from 'next/router'
 import { useMediaQuery } from '@mantine/hooks'
 import { callSetCourseMetadata } from '~/utils/apiUtils'
 import { montserrat_heading, montserrat_paragraph } from 'fonts'
-import SetWebScrapeParameters from './SetWebScrapeParameters'
 
 interface WebScrapeProps {
   is_new_course: boolean
@@ -48,6 +47,41 @@ export const WebScrape = ({
   const router = useRouter()
   const isSmallScreen = useMediaQuery('(max-width: 960px)')
   const theme = useMantineTheme()
+  const [inputList, setInputList] = useState(['', '', '']);
+  const [isTyping, setIsTyping] = useState(false)
+  const [stayOnBaseUrl, setStayOnBaseUrl] = useState(false)
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const value = e.target.value
+    const list = [...inputList]
+    list[index] = value
+    setInputList(list)
+    if (index === inputList.length - 1 && value !== '') {
+      setIsTyping(true)
+    }
+  }
+
+  const handleInputFocus = (index: number) => {
+    if (inputList.every((item) => item !== '')) {
+      setIsTyping(true)
+    }
+  }
+
+  useEffect(() => {
+    if (isTyping) {
+      handleAddClick()
+      setIsTyping(false)
+    }
+  }, [isTyping])
+
+  const handleAddClick = () => {
+    if (inputList[inputList.length - 1] !== '') {
+      setInputList([...inputList, ''])
+    }
+  }
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newUrl = e.target.value
@@ -337,9 +371,79 @@ export const WebScrape = ({
         rightSectionWidth={isSmallScreen ? 'auto' : 'auto'}
       />
       <div style={{ width: '50%' }}>
-        <SetWebScrapeParameters
-          course_name={courseName}
+      <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <Tooltip arrowPosition="side" arrowSize={8} withArrow position="bottom-start" label="Enter the maximum number of URLs to scrape">
+        <TextInput
+          label="Max URLs"
+          name="maximumUrls"
+          placeholder="Default 100"
+          value={inputList[0]}
+          onChange={(e) => {
+            const value = e.target.value;
+            const intValue = parseInt(value);
+            if (!isNaN(intValue) || value === '') {
+              handleInputChange(e, 0);
+            } else {
+              alert("Please enter a valid integer for Max URLs (numbers only)");
+            }
+          }}
+          onFocus={() => handleInputFocus(0)}
+          style={{ width: '100%' }}
         />
+      </Tooltip>
+      <Tooltip arrowPosition="side" arrowSize={8} withArrow position="bottom-start" label="Enter the timeout duration in seconds">
+        <TextInput
+          label="Timeout"
+          name="timeout"
+          placeholder="Default 1"
+          value={inputList[1]}
+          onChange={(e) => {
+            const value = e.target.value;
+            const intValue = parseInt(value);
+            if (!isNaN(intValue) || value === '') {
+              handleInputChange(e, 1);
+            } else {
+              alert("Please enter a valid integer for Timeout (numbers only)");
+            }
+          }}  
+          onFocus={() => handleInputFocus(1)}  
+          style={{ width: '100%' }}
+        />
+      </Tooltip>
+      <Tooltip arrowPosition="side" arrowSize={8} withArrow position="bottom-start" label="Enter the maximum depth for recursive scraping">
+        <TextInput
+          label="Max Depth"
+          name="maxDepth"
+          placeholder="Default 3"
+          value={inputList[2]}
+          onChange={(e) => {
+            const value = e.target.value;
+            const intValue = parseInt(value);
+            if (!isNaN(intValue) || value === '') {
+              handleInputChange(e, 2);
+            } else {
+              alert("Please enter a valid integer for Timeout (numbers only)");
+            }
+          }}
+          onFocus={() => handleInputFocus(2)}
+          style={{ width: '100%' }}
+        />
+      </Tooltip>
+      <div style={{ fontSize: 'smaller', marginBottom: '0px' }}>
+          Stay on Base URL
+      </div>
+      <Tooltip arrowPosition="side" arrowSize={8} withArrow position="bottom-start" label="Only Scrape Information from the Base URL">
+        <Checkbox 
+          checked={stayOnBaseUrl}
+          size="md"
+          onChange={() => setStayOnBaseUrl(!stayOnBaseUrl)}
+        />
+      </Tooltip>
+    </form>
       </div>
       <Text
         size={rem(14)}
