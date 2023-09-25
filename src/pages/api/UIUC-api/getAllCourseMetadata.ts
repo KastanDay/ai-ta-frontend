ca9a15c94ae13a0811a0189b69de4789dfba2bfc
Kastan Day <kastanvday@gmail.com>
2023-09-25 15:24:48 -0700

    working getAllCourseMetadata api

--- src/pages/api/UIUC-api/getAllCourseMetadata.ts
+++ src/pages/api/UIUC-api/getAllCourseMetadata.ts
@@ -1,4 +1,4 @@
-// ~/src/pages/api/UIUC-api/getCourseMetadata.ts
+// ~/src/pages/api/UIUC-api/getAllCourseMetadata.ts
 import { kv } from '@vercel/kv'
 import { NextResponse } from 'next/server'
 import { CourseMetadata } from '~/types/courseMetadata'
@@ -6,10 +6,20 @@ import { log } from 'next-axiom'
 
 export const runtime = 'edge'
 
-// TODO work in progress
+// USAGE:
+// useEffect(() => {
+//   const fetchCourseMetadata = async () => {
+//     const response = await fetch(
+//       `/api/UIUC-api/getAllCourseMetadata`,
+//     )
+//     const all_course_metadata = await response.json()
+//     console.log("All Course metadta: ", all_course_metadata)
+//   }
 
-export const getAllCourseMetadata = async (
-): Promise<CourseMetadata[] | null> => {
+//   fetchCourseMetadata()
+// }, [])
+
+export const getAllCourseMetadata = async (): Promise<CourseMetadata[] | null> => {
   try {
     // const all_course_metadata = await kv.hgetall(
     //   'course_metadatas',
@@ -18,9 +28,19 @@ export const getAllCourseMetadata = async (
     const all_course_metadata_raw = await kv.hgetall('course_metadatas')
 
     if (all_course_metadata_raw) {
-      const all_course_metadata = Object.values(all_course_metadata_raw).map(item => item as CourseMetadata)
+      const all_course_metadata = Object.values(all_course_metadata_raw).map(item => {
+        const courseMetadata = item as CourseMetadata;
+        if (!courseMetadata) {
+          console.error('Invalid course metadata:', item);
+          return null;
+        }
+        return courseMetadata;
+      }).filter(item => item !== null) as CourseMetadata[];
       console.log('in direct course name', all_course_metadata)
       return all_course_metadata
+    } else {
+      console.error('Error occurred while fetching courseMetadata: No course metadata found for ANY course!')
+      return null
     }
 
   } catch (error) {
@@ -30,68 +50,16 @@ export const getAllCourseMetadata = async (
 }
 
 export default async (req: any, res: any) => {
-  const course_name = req.nextUrl.searchParams.get('course_name')
-  const course_metadata = await getCourseMetadata(course_name)
-
   try {
-    if (course_metadata == null) {
-      return NextResponse.json({
-        success: true,
-        course_metadata: null,
-        course_exists: false,
-      })
-    }
-
-    // Only parse is_private if it exists
-    if (course_metadata.hasOwnProperty('is_private')) {
-      course_metadata.is_private = JSON.parse(
-        course_metadata.is_private as unknown as string,
-      )
-    }
+    const all_course_metadata = await getAllCourseMetadata()
     log.debug('getCourseMetadata() success', {
-      course_metadata: course_metadata,
+      all_course_metadata: all_course_metadata,
     })
-    return NextResponse.json({ course_metadata: course_metadata })
+    return NextResponse.json({ course_metadata: all_course_metadata })
+
   } catch (error) {
     console.log('Error occurred while fetching courseMetadata', error)
     log.error('Error occurred while fetching courseMetadata', { error: error })
     return NextResponse.json({ success: false, error: error })
   }
 }
-
-// const getCourseMetadata = async (req: any, res: any) => {
-//   const course_name = req.nextUrl.searchParams.get('course_name')
-//   log.debug('getCourseMetadata() request', { course_name: course_name })
-//   try {
-//     const course_metadata = (await kv.hget(
-//       'course_metadatas',
-//       course_name,
-//     )) as CourseMetadata
-//     // console.log('in api getCourseMetadata: course_metadata', course_metadata)
-
-//     if (course_metadata == null) {
-//       return NextResponse.json({
-//         success: true,
-//         course_metadata: null,
-//         course_exists: false,
-//       })
-//     }
-
-//     // Only parse is_private if it exists
-//     if (course_metadata.hasOwnProperty('is_private')) {
-//       course_metadata.is_private = JSON.parse(
-//         course_metadata.is_private as unknown as string,
-//       )
-//     }
-//     log.debug('getCourseMetadata() success', {
-//       course_metadata: course_metadata,
-//     })
-//     return NextResponse.json({ course_metadata: course_metadata })
-//   } catch (error) {
-//     console.log('Error occurred while fetching courseMetadata', error)
-//     log.error('Error occurred while fetching courseMetadata', { error: error })
-//     return NextResponse.json({ success: false, error: error })
-//   }
-// }
-
-// export default getCourseMetadata
