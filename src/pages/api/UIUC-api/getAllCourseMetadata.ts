e395231b32a0c1a319a7d95d24823196d67bd796
Kastan Day <kastanvday@gmail.com>
2023-09-25 17:47:59 -0700

    filter allCourseMetadata before returning to client so we don't leak info to users

--- src/pages/api/UIUC-api/getAllCourseMetadata.ts
+++ src/pages/api/UIUC-api/getAllCourseMetadata.ts
@@ -19,8 +19,61 @@ export const runtime = 'edge'
 //   fetchCourseMetadata()
 // }, [])
 
+export const getCoursesByOwnerOrAdmin = async (
+  currUserEmail: string,
+): Promise<{ [key: string]: CourseMetadata }[] | null> => {
+  /*
+  I return a list of dictionaries of course metadata where the currUserEmail is an owner or admin
+    The key is the courseName
+    The value is the CourseMetadata object
+  */
 
-export const getAllCourseMetadata = async (): Promise<{ [key: string]: CourseMetadata }[] | null> => {
+  try {
+    const all_course_metadata_raw = await kv.hgetall('course_metadatas')
+    console.log('Raw metadata')
+    // console.log(all_course_metadata_raw)
+
+    if (all_course_metadata_raw) {
+      const all_course_metadata = Object.entries(all_course_metadata_raw)
+        .map(([key, value]) => {
+          const courseMetadata = value as CourseMetadata
+          if (!courseMetadata) {
+            console.error('Invalid course metadata:', value)
+            return null
+          }
+          // Check if the current user is the course owner or a course admin
+          if (
+            courseMetadata.course_owner &&
+            courseMetadata.course_admins &&
+            (courseMetadata.course_owner === currUserEmail ||
+              courseMetadata.course_admins.includes(currUserEmail))
+          ) {
+            console.log('Found a course: ', courseMetadata)
+            return { [key]: courseMetadata }
+          }
+          // return null;
+        })
+        .filter(
+          (item) =>
+            item !== null && item !== undefined && Object.keys(item).length > 0,
+        ) as { [key: string]: CourseMetadata }[]
+      console.log('in direct course name', all_course_metadata)
+      return all_course_metadata
+    } else {
+      console.error(
+        'Error occurred while fetching courseMetadata: No course metadata found for ANY course!',
+      )
+      return null
+    }
+  } catch (error) {
+    console.error('Error occurred while fetching courseMetadata', error)
+    return null
+  }
+}
+
+export const getAllCourseMetadata = async (): Promise<
+  { [key: string]: CourseMetadata }[] | null
+> => {
   /*
   I return a list of dictionaries of course metadata
     The key is the courseName
@@ -29,25 +82,28 @@ export const getAllCourseMetadata = async (): Promise<{ [key: string]: CourseMet
 
   try {
     const all_course_metadata_raw = await kv.hgetall('course_metadatas')
-    console.log("Raw metadata")
+    console.log('Raw metadata')
     console.log(all_course_metadata_raw)
 
     if (all_course_metadata_raw) {
-      const all_course_metadata = Object.entries(all_course_metadata_raw).map(([key, value]) => {
-        const courseMetadata = value as CourseMetadata;
-        if (!courseMetadata) {
-          console.error('Invalid course metadata:', value);
-          return null;
-        }
-        return { [key]: courseMetadata };
-      }).filter(item => item !== null) as { [key: string]: CourseMetadata }[];
+      const all_course_metadata = Object.entries(all_course_metadata_raw)
+        .map(([key, value]) => {
+          const courseMetadata = value as CourseMetadata
+          if (!courseMetadata) {
+            console.error('Invalid course metadata:', value)
+            return null
+          }
+          return { [key]: courseMetadata }
+        })
+        .filter((item) => item !== null) as { [key: string]: CourseMetadata }[]
       console.log('in direct course name', all_course_metadata)
       return all_course_metadata
     } else {
-      console.error('Error occurred while fetching courseMetadata: No course metadata found for ANY course!')
+      console.error(
+        'Error occurred while fetching courseMetadata: No course metadata found for ANY course!',
+      )
       return null
     }
-
   } catch (error) {
     console.error('Error occurred while fetching courseMetadata', error)
     return null
@@ -56,12 +112,14 @@ export const getAllCourseMetadata = async (): Promise<{ [key: string]: CourseMet
 
 export default async (req: any, res: any) => {
   try {
-    const all_course_metadata = await getAllCourseMetadata()
+    console.log(req)
+    // const currUserEmail = req.query.currUserEmail;
+    const currUserEmail = req.nextUrl.searchParams.get('currUserEmail')
+    const all_course_metadata = await getCoursesByOwnerOrAdmin(currUserEmail)
     log.debug('getCourseMetadata() success', {
       all_course_metadata: all_course_metadata,
     })
-    return NextResponse.json({ course_metadata: all_course_metadata })
-
+    return NextResponse.json(all_course_metadata)
   } catch (error) {
     console.log('Error occurred while fetching courseMetadata', error)
     log.error('Error occurred while fetching courseMetadata', { error: error })
