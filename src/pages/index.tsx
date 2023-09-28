aa4b93cda7f4cc0e0817cbcf0fbd54aa0a1df9d7
Kastan Day <kastanvday@gmail.com>
2023-09-27 22:17:11 -0700

    Add 'New Project' button on landing page (#44)
    * update all railway calls to use Staging
    
    * remove 'Coming soon' from home page
    
    * move railway url to staging for /ingest api
    
    * starting to add toasts to failed file ingests
    
    * remove all trailing slashes from flask app URL
    
    * ALWAYS auto create new convo
    
    * ALWAYS auto create new convo
    
    * FORMAT FIX ONLY
    
    * move RAILWAY_URL into environment variable for staging vs production seamlessness
    
    * Fix railway url formatting as env var
    
    * switch back to hard coded railway urls...
    
    * todo next, open toast for failure ingests
    
    * saving toasts for another day, it's so close but not there
    
    * very nice 'New Project' button on landing page
    
    * fix icon pack
    
    * fix old imports
    
    * fix spacing on global header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ import {
   Group,
 } from '@mantine/core'
 
-import GlobalHeader from '~/components/UIUC-Components/GlobalHeader'
+import GlobalHeader, { LandingPageHeader } from '~/components/UIUC-Components/GlobalHeader'
 import GlobalFooter from '~/components/UIUC-Components/GlobalFooter'
 
 const Home: NextPage = () => {
@@ -29,14 +29,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <GlobalHeader />
+      <LandingPageHeader />
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#0E1116]">
         <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            {/* <Link href="/"> */}
             UIUC.<span className="text-[hsl(280,100%,70%)]">chat</span>
-            {/* </Link> */}
           </h1>
 
           <Container
@@ -171,9 +169,8 @@ const useStyles = createStyles((theme) => ({
   },
 
   card: {
-    border: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
-    }`,
+    border: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+      }`,
   },
 
   cardTitle: {
