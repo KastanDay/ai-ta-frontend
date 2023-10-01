1f7ec18e711b21df06efe0dab3ec9412bc2b3c78
Maxwell-Lindsey <mal13@illinois.edu>
2023-10-01 17:08:24 -0500

    fixed type error for vercel check

--- src/components/UIUC-Components/WebScrape.tsx
+++ src/components/UIUC-Components/WebScrape.tsx
@@ -197,7 +197,7 @@ export const WebScrape = ({
   };
 
   const validateInputs = () => {
-    let errors = {
+    const errors = {
       maxUrls: { error: false, message: '' },
       maxDepth: { error: false, message: '' }
     };
