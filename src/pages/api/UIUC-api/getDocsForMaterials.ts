6d3005bc7bf07f5c001eb8c4f989a1f8b36ffd3e
hanlily666 <hanruixin1999@163.com>
2023-11-06 15:11:48 -0600

    using variable to connect supabse

--- src/pages/api/UIUC-api/getDocsForMaterials.ts
+++ src/pages/api/UIUC-api/getDocsForMaterials.ts
@@ -12,7 +12,10 @@ import { createClient } from '@supabase/supabase-js'
 
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+)
 
 interface CourseDocuments {
   readable_filename: string;
