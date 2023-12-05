48b174d1798d3cf579fb28d904e2d39eb0a57156
Rohan Marwaha <rohan13@illinois.edu>
2023-12-05 12:01:47 -0600

    Adding a deep equality check to handle infinite loop in memo caused by strict equality check on objects instead of values

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -146,6 +146,30 @@ export const ChatMessage: FC<Props> = memo(
       }
     }, [message.role, messageIsStreaming, messageIndex, selectedConversation])
 
+    function deepEqual(a: any, b: any) {
+      if (a === b) {
+        return true;
+      }
+
+      if (typeof a !== 'object' || a === null || typeof b !== 'object' || b === null) {
+        return false;
+      }
+
+      let keysA = Object.keys(a), keysB = Object.keys(b);
+
+      if (keysA.length !== keysB.length) {
+        return false;
+      }
+
+      for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
     useEffect(() => {
       const fetchUrl = async () => {
         let isValid = false;
@@ -167,7 +191,7 @@ export const ChatMessage: FC<Props> = memo(
             }
             return content;
           }));
-          if (!isValid && onImageUrlsUpdate && updatedContent !== message.content) {
+          if (!isValid && onImageUrlsUpdate && !deepEqual(updatedContent, message.content)) {
             console.log("Updated content: ", updatedContent, "Previous content: ", message.content)
             onImageUrlsUpdate({ ...message, content: updatedContent }, messageIndex);
           }
