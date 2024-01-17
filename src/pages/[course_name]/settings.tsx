0b0754537ac91da7321b57965f7264d4c33f5421
hanlily666 <hanruixin1999@163.com>
2024-01-17 16:37:58 -0600

    got the updated coursemetadata

--- src/pages/[course_name]/settings.tsx
+++ src/pages/[course_name]/settings.tsx
@@ -4,6 +4,8 @@ import router from "next/router";
 import { DEFAULT_SYSTEM_PROMPT } from '~/utils/app/const';
 import { callSetCourseMetadata } from '~/utils/apiUtils';
 import { getAllCourseMetadata } from '../api/UIUC-api/getAllCourseMetadata';
+import { extractEmailsFromClerk } from '~/components/UIUC-Components/clerkHelpers';
+import { useUser } from '@clerk/nextjs';
 
 interface SettingsProps {
   t: (key: string) => string
@@ -13,17 +15,25 @@ const getCurrentCourseName = () => {
   return router.asPath.split('/')[1];
 }
 
+
 const Settings = ({ t }: SettingsProps) => {
   const [systemPrompt, setSystemPrompt] = useState(DEFAULT_SYSTEM_PROMPT);
   const [courseMetadata, setCourseMetadata] = useState<CourseMetadata | null>(null);
+  const clerk_user = useUser()
+  const emails = extractEmailsFromClerk(clerk_user.user)
+  const currUserEmail = emails[0]
 
   const fetchCourseMetadata = async () => {
     const courseName = getCurrentCourseName();
+
     if (!courseName) {
       throw new Error('Course name is undefined');
     }
-    const allMetadata = await getAllCourseMetadata();
-    const metadata = allMetadata?.find(meta => Object.keys(meta)[0] === courseName);
+    const response = await fetch(`/api/UIUC-api/getAllCourseMetadata?currUserEmail=${currUserEmail}`);
+    const allMetadata = await response.json();
+    console.log(allMetadata);
+    const metadataObj = allMetadata?.find((meta: { [key: string]: CourseMetadata }) => Object.keys(meta)[0] === courseName) || null;
+    const metadata = metadataObj ? Object.values(metadataObj)[0] as CourseMetadata : null;
     setCourseMetadata(metadata);
     if (metadata) {
       setSystemPrompt(metadata.system_prompt || DEFAULT_SYSTEM_PROMPT);
