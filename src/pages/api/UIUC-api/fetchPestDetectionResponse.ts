395b20b753b6e36e9dd1cea7ef85c253b3e3ca26
rohanmarwaha <rohan13@illinois.edu>
2024-02-16 02:48:32 +0530

    Added docstring ONLY

--- src/pages/api/UIUC-api/fetchPestDetectionResponse.ts
+++ src/pages/api/UIUC-api/fetchPestDetectionResponse.ts
@@ -3,6 +3,12 @@ export const config = {
   runtime: 'edge',
 }
 
+/**
+ * Fetches the pest detection response from the pest detection API.
+ * @param imageUrls The image urls to send to the API
+ * @returns The response from the API as s3 paths for the annotated images for the detected pests.
+ */
+
 export const fetchPestDetectionResponse = async (imageUrls: string[]): Promise<string[]> => {
   try {
     const response = await fetch(`https://kastanday--v2-pest-detection-yolo-model-predict.modal.run/`, {
