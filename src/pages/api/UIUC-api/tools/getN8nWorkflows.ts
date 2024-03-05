cd1ce1e650286e1638b2dc578ca7747d72d83419
jkmin3 <ijoshuamin@gmail.com>
2024-03-05 11:48:02 -0600

    better fetching for different format of workflow records

--- src/pages/api/UIUC-api/tools/getN8nWorkflows.ts
+++ src/pages/api/UIUC-api/tools/getN8nWorkflows.ts
@@ -1,6 +1,7 @@
 // ingest.ts
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios, { AxiosResponse } from 'axios'
+import { WorkflowRecord } from '~/types/tools'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -18,23 +19,32 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log('pagination', pagination)
 
     // localhost: 8000 / getworkflows ? api_key = n8n_api_304b9f5f0836aba9a8aa1c20fafbebfff49b2e1f2c2191c764aad26b614a19c320b0ffa041c0785f & limit=10 & pagination=True
-
-    const response: AxiosResponse = await axios.get(
-      // `https://flask-production-751b.up.railway.app/getworkflows`,
-      // ?api_key=n8n_api_304b9f5f0836aba9a8aa1c20fafbebfff49b2e1f2c2191c764aad26b614a19c320b0ffa041c0785f&limit=10&pagination=True
-      `http://localhost:8000/getworkflows`,
-      {
-        params: {
-          api_key: api_key,
-          limit: parsedLimit,
-          pagination: parsedPagination,
-        },
-      },
+    // const response: AxiosResponse = await axios.get(
+    //   // `https://flask-production-751b.up.railway.app/getworkflows`,
+    //   // ?api_key=n8n_api_304b9f5f0836aba9a8aa1c20fafbebfff49b2e1f2c2191c764aad26b614a19c320b0ffa041c0785f&limit=10&pagination=True
+    //   `http://localhost:8000/getworkflows`,
+    //   {
+    //     params: {
+    //       api_key: api_key,
+    //       limit: parsedLimit,
+    //       pagination: parsedPagination,
+    //     },
+    //   },
+    // )
+    const response = await fetch(
+      `http://localhost:8000/getworkflows?api_key=${api_key}&limit=${parsedLimit}&pagination=${parsedPagination}`,
     )
+    if (!response.ok) {
+      return res.status(response.status).json({ error: response.statusText })
+    }
     // This parses and simplifies the nested structure of the data...
-    const simplifiedData = response.data.map((workflow: any) => {
+    const data = await response.json()
+    console.log('response', data)
+    const simplifiedData = data.map((workflow: WorkflowRecord) => {
+      // workflow.tags = workflow.tags.name
       return workflow
     })
+    console.log('simplifiedData', simplifiedData)
     const final_data = simplifiedData[0]
     return res.status(200).json(final_data)
     // console.log('Getting to our /ingest endpoint', data);
