336dd4b1f0c9f8bd5373524ffe60858c96ad3e79
Kastan Day <kastanvday@gmail.com>
2024-03-06 08:08:49 -0800

    Initial attempt, not sure about edge runtime

--- src/pages/api/UIUC-api/ingest.ts
+++ src/pages/api/UIUC-api/ingest.ts
@@ -1,6 +1,9 @@
-// ingest.ts
 import { NextApiRequest, NextApiResponse } from 'next'
-import axios, { AxiosResponse } from 'axios'
+// import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  runtime: 'edge',
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -10,20 +13,44 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       readableFilename: string
     }
 
+    // Parse the URL to get query parameters
+    // const url = new URL(req.url)
+    // const uniqueFileName = url.searchParams.get('uniqueFileName')
+    // const courseName = url.searchParams.get('courseName')
+    // const readableFilename = url.searchParams.get('readableFilename')
+
+    if (!uniqueFileName || !courseName || !readableFilename) {
+      // Handle the case where any of the parameters are missing
+      console.error('Missing query parameters')
+      return res.status(400).json({ error: 'Missing query parameters' })
+      // return new Response(JSON.stringify({ error: 'Missing query parameters' }), { status: 400 })
+    }
+
     const s3_filepath = `courses/${courseName}/${uniqueFileName}`
 
-    const response: AxiosResponse = await axios.get(
-      `https://flask-production-751b.up.railway.app/ingest`,
-      {
-        params: {
-          course_name: courseName,
-          s3_paths: s3_filepath,
-          readable_filename: readableFilename,
-        },
+    fetch('https://41kgx.apps.beam.cloud', {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        Authorization: `Basic ${process.env.BEAM_API_KEY}`,
+        'Content-Type': 'application/json',
       },
-    )
-    // const data = await
-    return res.status(200).json(response.data)
+      body: JSON.stringify({
+        course_name: courseName,
+        readable_filename: readableFilename,
+        s3_paths: s3_filepath,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`In success case -- Data ingested for URL: ${s3_filepath}`)
+        res.status(200).json(data)
+      })
+      .catch((err) => {
+        console.error(err)
+        res.status(500).json({ error: 'Internal Server Error' })
+      })
     // console.log('Getting to our /ingest endpoint', data);
     // return data;
   } catch (error) {
