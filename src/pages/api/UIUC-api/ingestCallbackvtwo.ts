32c93d320dc0f3b16fbcae0e0daebd47e8da012e
Kastan Day <kastanvday@gmail.com>
2024-03-07 10:49:54 -0800

    Trying new method from https://github.com/vercel/next.js/discussions/48427\#discussioncomment-6028533

---
+++ src/pages/api/UIUC-api/ingestCallbackvtwo.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import EventEmitter from 'events'
+
+export const delay = (ms: number) =>
+  new Promise<void>((resolve) => setTimeout(resolve, ms))
+
+const stream = new EventEmitter()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache, no-transform')
+  res.setHeader('Connection', 'keep-alive')
+
+  let counter = 0
+
+  stream.on('channel', function (event, data) {
+    //res.write(JSON.stringify({ counter: data })); // NOTE: this DOES NOT work
+    res.write(`event: ${event}\ndata: ${JSON.stringify({ counter: data })}\n\n`) // <- the format here is important!
+  })
+
+  for (let i = 0; i < 10; ++i) {
+    stream.emit('channel', 'myEventName', counter) // the event name here must be the same as in the EventSource in frontend
+    console.log('update counter', counter)
+    counter++
+    await delay(1000)
+  }
+  res.end('done\n')
+}
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,14 +1,53 @@
 import { NextPage } from 'next'
-import { MainPageBackground } from '~/components/UIUC-Components/MainPageBackground'
-import TestEventSourceComponent from '~/components/UIUC-Components/TestEventSourceComponent'
+import { useState } from 'react'
 
-const Events: NextPage = () => {
-  console.log('In Events.tsx')
+const Page: NextPage = () => {
+  const [myEventNameData, setMyEventNameData] = useState<Array<string>>([])
+
+  const callSSE = () => {
+    console.log('In callSSE')
+    const eventSource = new EventSource(`/api/UIUC-api/ingestCallbackvtwo`)
+    eventSource.addEventListener('myEventName', (e) => {
+      console.log('in myEventName event listener', e.data)
+      // the event name here must be the same as in the API
+      const parsedData = JSON.parse(e.data)
+      console.log(parsedData)
+      setMyEventNameData((prevData) => [...prevData, parsedData])
+    })
+    eventSource.addEventListener('open', (e) => {
+      console.log('open', e)
+    })
+    eventSource.addEventListener('error', (e) => {
+      console.log('error', e)
+      eventSource.close()
+    })
+  }
+
   return (
-    <MainPageBackground>
-      <TestEventSourceComponent />
-    </MainPageBackground>
+    <div>
+      <button onClick={callSSE}>GET SSE</button>
+      <ol>
+        {myEventNameData.map((data, index) => (
+          <li key={index}>{JSON.stringify(data)}</li>
+        ))}
+      </ol>
+    </div>
   )
 }
 
-export default Events
+export default Page
+
+// import { NextPage } from 'next'
+// import { MainPageBackground } from '~/components/UIUC-Components/MainPageBackground'
+// import TestEventSourceComponent from '~/components/UIUC-Components/TestEventSourceComponent'
+
+// const Events: NextPage = () => {
+//   console.log('In Events.tsx')
+//   return (
+//     <MainPageBackground>
+//       <TestEventSourceComponent />
+//     </MainPageBackground>
+//   )
+// }
+
+// export default Events
