a4ee8466c0f8d0fc23d46405ad6928ba19e72745
Kastan Day <kastanvday@gmail.com>
2024-03-07 11:07:20 -0800

    Try changing headers

--- src/pages/api/UIUC-api/ingestCallbackvtwo.ts
+++ src/pages/api/UIUC-api/ingestCallbackvtwo.ts
@@ -1,3 +1,45 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import EventEmitter from "events";
+// import ioredis from "ioredis";
+
+// const stream = new EventEmitter();
+
+// const redis = new ioredis({
+//   host: "127.0.0.1",
+//   port: 6379,
+//   password: "",
+//   family: 4,
+//   db: 0,
+// });
+
+// redis.subscribe("test");
+
+// export const config = {
+//   api: {
+//     externalResolver: true,
+//   },
+// }; // this is important to avoid the 'API resolved without sending a response for /api/test_sse, this may result in stalled requests.' warning
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+//   res.setHeader("Content-Type", "text/event-stream");
+//   res.setHeader("Cache-Control", "no-cache, no-transform");
+//   res.setHeader("Connection", "keep-alive");
+//   res.setHeader('Content-Encoding', 'none')
+//   res.flushHeaders()
+
+//   stream.on("channel", function (event, data) {
+//     res.write(`event: ${event}\ndata: ${JSON.stringify({ counter: data })}\n\n`); // <- the format here is important!
+//   });
+
+//   redis.on("message", (channel: string, message: string) => {
+//     console.log(`get ${message} on ${channel}`);
+//     stream.emit("channel", "myEventName", message);
+//   });
+
+//   redis.on("close", () => res.end());
+// }
+
+// NOT WORKING ON VERCEL
 import type { NextApiRequest, NextApiResponse } from 'next'
 import EventEmitter from 'events'
 
@@ -13,6 +55,8 @@ export default async function handler(
   res.setHeader('Content-Type', 'text/event-stream')
   res.setHeader('Cache-Control', 'no-cache, no-transform')
   res.setHeader('Connection', 'keep-alive')
+  res.setHeader('Content-Encoding', 'none')
+  res.flushHeaders()
 
   let counter = 0
 
