b40e6e517e57caea54f4f194bc7d9690feaf357b
Kastan Day <kastanvday@gmail.com>
2024-03-08 13:37:45 -0800

    Fix sanitization to use all dashes, no dependencies either

--- src/components/UIUC-Components/LargeDropzone.tsx
+++ src/components/UIUC-Components/LargeDropzone.tsx
@@ -1,6 +1,5 @@
 // LargeDropzone.tsx
 import React, { useRef, useState } from 'react'
-import sanitize from 'sanitize-filename'
 import {
   createStyles,
   Group,
@@ -164,7 +163,7 @@ export function LargeDropzone({
     const allSuccessOrFail = await Promise.all(
       files.map(async (file, index) => {
         console.log('Index: ' + index)
-        const filename = sanitize(file.name)
+        const filename = file.name.replace(/[^a-zA-Z0-9]/g, '-') // only letters and numbers
         const uniqueFileName = (uuidv4() as string) + '-' + filename
 
         // return { ok: Math.random() < 0.5, s3_path: filename }; // For testing
