702a822a5d8ec8437f2b8af2421ce6ba6690ef75
Kastan Day <kastanvday@gmail.com>
2024-03-12 11:24:10 -0600

    Fix sanitization to preserve file extension

--- src/components/UIUC-Components/LargeDropzone.tsx
+++ src/components/UIUC-Components/LargeDropzone.tsx
@@ -163,8 +163,12 @@ export function LargeDropzone({
     const allSuccessOrFail = await Promise.all(
       files.map(async (file, index) => {
         console.log('Index: ' + index)
-        const filename = file.name.replace(/[^a-zA-Z0-9]/g, '-') // only letters and numbers
-        const uniqueFileName = (uuidv4() as string) + '-' + filename
+        // Sanitize the filename and retain the extension
+        const extension = file.name.slice(file.name.lastIndexOf('.'))
+        const nameWithoutExtension = file.name
+          .slice(0, file.name.lastIndexOf('.'))
+          .replace(/[^a-zA-Z0-9]/g, '-')
+        const uniqueFileName = `${uuidv4()}-${nameWithoutExtension}${extension}`
 
         // return { ok: Math.random() < 0.5, s3_path: filename }; // For testing
         try {
