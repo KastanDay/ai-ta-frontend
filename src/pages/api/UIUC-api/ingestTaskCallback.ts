e1e2f3a4b9be1f024dc1b67329c1ffbdc893491c
Kastan Day <kastanvday@gmail.com>
2024-03-18 17:44:10 -0700

    Clean up prints

--- src/pages/api/UIUC-api/ingestTaskCallback.ts
+++ src/pages/api/UIUC-api/ingestTaskCallback.ts
@@ -10,9 +10,6 @@ export const config = {
 // The beam function handles sending to Success and Failure cases. We just handle removing from in-progress.
 const handler = async (req: NextApiRequest) => {
   try {
-    console.log('ingestTaskCallback.ts: handler: req.method:', req.method)
-    console.log('ingestTaskCallback.ts: handler: req.body:', req.body)
-
     if (req.method !== 'POST') {
       console.error('Request method not allowed')
       return NextResponse.json(
@@ -25,25 +22,19 @@ const handler = async (req: NextApiRequest) => {
     // First, we convert the stream into a Response object, then use .json() to parse it.
     const datastr = await new Response(req.body).json()
     const data = JSON.parse(datastr)
-    console.log('Data: ', data)
-    console.log('Type of data:', typeof data)
-    // Data: {"success_ingest": "courses/t/7918ea72-32b6-45b1-aec7-0da1c68b3555-1.txt", "failure_ingest": []}
-    console.log('Success ingest:', data.success_ingest)
-    console.log('Failure ingest:', data.failure_ingest)
-    // const combinedIngests: string[] = []
+    // Data:  {
+    //   success_ingest: 'courses/t/8885632f-b519-4610-b888-744aa4c2066d-6.pdf',
+    //   failure_ingest: null
+    // }
+
     let s3_path_completed = ''
     if (data.success_ingest) {
       console.log('IN SUCCESS INGEST ___:', data.success_ingest)
-      // combinedIngests.push(data.success_ingest)
       s3_path_completed = data.success_ingest
     }
     if (data.failure_ingest) {
-      // combinedIngests.push(data.failure_ingest.s3_path)
       s3_path_completed = data.failure_ingest.s3_path
     }
-    // const s3_path_completed = combinedIngests[0]
-    // console.log('Combined ingests:', combinedIngests)
-    console.log('s3_path_completed:', s3_path_completed)
 
     // Remove from in progress
     if (s3_path_completed) {
@@ -64,7 +55,7 @@ const handler = async (req: NextApiRequest) => {
         })
       }
     } else {
-      console.log(`No success ingest in Beam data: ${data}}`)
+      console.error(`No success/failure ingest in Beam callback data: ${data}}`)
     }
 
     return NextResponse.json({ message: 'Success' }, { status: 200 })
