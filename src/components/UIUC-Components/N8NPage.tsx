bf2b461584eeef3847e96cbf51e43b4bd5c8aa66
Kastan Day <kastanvday@gmail.com>
2024-03-21 10:47:51 -0700

    Major cleanup of N8N page. Big improvement to upsert API key

--- src/components/UIUC-Components/N8NPage.tsx
+++ src/components/UIUC-Components/N8NPage.tsx
@@ -1,71 +1,34 @@
-import Head from 'next/head'
-import { montserrat_heading, montserrat_paragraph } from 'fonts'
-// import { DropzoneS3Upload } from '~/components/UIUC-Components/Upload_S3'
-import { fetchPresignedUrl } from '~/utils/apiUtils'
 import {
-  // Badge,
-  // MantineProvider,
   Button,
-  // Group,
-  // Stack,
-  // createStyles,
-  // FileInput,
-  // rem,
   Title,
   Flex,
-  createStyles,
-  // Divider,
-  MantineTheme,
   List,
-  Text,
+  // Text,
   TextInput,
-  // TextInput,
-  // Tooltip,
 } from '@mantine/core'
-// const rubik_puddles = Rubik_Puddles({ weight: '400', subsets: ['latin'] })
+
+import {
+  IconAlertCircle,
+  IconCheck,
+  IconExternalLink,
+} from '@tabler/icons-react'
+
+import { CannotEditCourse } from './CannotEditCourse'
+import { type CourseMetadata } from '~/types/courseMetadata'
+
+import { MainPageBackground } from './MainPageBackground'
+import { extractEmailsFromClerk } from './clerkHelpers'
+import { notifications } from '@mantine/notifications'
+import GlobalFooter from './GlobalFooter'
+import Navbar from './navbars/Navbar'
+import { N8nWorkflowsTable } from './N8nWorkflowsTable'
+
+import Head from 'next/head'
+import { montserrat_heading, montserrat_paragraph } from 'fonts'
+import { fetchCourseMetadata } from '~/utils/apiUtils'
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { useRouter } from 'next/router'
 import { LoadingSpinner } from './LoadingSpinner'
-import { supabase } from '@/utils/supabaseClient'
-import { Accordion } from '@mantine/core'
-
-const useStyles = createStyles((theme: MantineTheme) => ({
-  downloadButton: {
-    fontFamily: 'var(--font-montserratHeading)',
-    outline: 'none',
-    border: 'solid 1.5px',
-    borderColor: theme.colors.grape[8],
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: theme.spacing.xs,
-    borderRadius: theme.radius.xl,
-    cursor: 'pointer',
-    transition: 'background-color 0.2s ease-in-out',
-    height: '48px',
-    backgroundColor: '#0E1116',
-
-    '&:hover': {
-      backgroundColor: theme.colors.grape[8],
-    },
-    '@media (max-width: 768px)': {
-      fontSize: theme.fontSizes.xs,
-      padding: '10px',
-      width: '70%',
-    },
-    '@media (min-width: 769px) and (max-width: 1024px)': {
-      fontSize: theme.fontSizes.xs,
-      padding: '12px',
-      width: '90%',
-    },
-    '@media (min-width: 1025px)': {
-      fontSize: theme.fontSizes.sm,
-      padding: '15px',
-      width: '100%',
-    },
-  },
-}))
-
 import { useAuth, useUser } from '@clerk/nextjs'
 
 export const GetCurrentPageName = () => {
@@ -77,12 +40,15 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
   // Check auth - https://clerk.com/docs/nextjs/read-session-and-user-data
   const { isLoaded, userId, sessionId, getToken } = useAuth() // Clerk Auth
   // const { isSignedIn, user } = useUser()
+  const router = useRouter()
+  const currentPageName = GetCurrentPageName()
   const clerk_user = useUser()
   const [courseMetadata, setCourseMetadata] = useState<CourseMetadata | null>(
     null,
   )
   const [currentEmail, setCurrentEmail] = useState('')
   const [n8nApiKey, setN8nApiKey] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSaveApiKey = async () => {
     console.log('Saving n8n API Key:', n8nApiKey)
@@ -125,13 +91,8 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
     }
     setIsLoading(false)
   }
-  const router = useRouter()
 
-  const currentPageName = GetCurrentPageName()
-
-  const [isLoading, setIsLoading] = useState(false)
-
-  // TODO: remove this hook... we should already have this from the /materials props???
+  // TODO: use react query hook?
   useEffect(() => {
     const fetchData = async () => {
       const userEmail = extractEmailsFromClerk(clerk_user.user)
@@ -157,33 +118,6 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
     fetchData()
   }, [currentPageName, clerk_user.isLoaded])
 
-  const [nomicMapData, setNomicMapData] = useState<NomicMapData | null>(null)
-  const [nomicIsLoading, setNomicIsLoading] = useState(true)
-
-  // fetch nomicMapData
-  useEffect(() => {
-    const fetchNomicMapData = async () => {
-      try {
-        console.log('Trying to fetch nomic!!')
-        const response = await fetch(
-          `/api/getNomicMapForQueries?course_name=${course_name}`,
-        )
-        const data = await response.json()
-        const parsedData: NomicMapData = {
-          map_id: data.map_id,
-          map_link: data.map_link,
-        }
-        setNomicMapData(parsedData)
-        setNomicIsLoading(false)
-      } catch (error) {
-        console.error('Error fetching nomic map:', error)
-        setNomicIsLoading(false) // Set nomicIsLoading to false even if there is an error
-      }
-    }
-
-    fetchNomicMapData()
-  }, [course_name])
-
   if (!isLoaded || !courseMetadata) {
     return (
       <MainPageBackground>
@@ -192,7 +126,7 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
     )
   }
 
-  // TODO: update this check to consider Admins & participants.
+  // Check auth
   if (
     courseMetadata &&
     currentEmail !== (courseMetadata.course_owner as string) &&
@@ -206,44 +140,6 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
         // current_email={currentEmail as string}
       />
     )
-  }
-
-  // const upsertN8nApiKey = async (newN8nApiKey: string) => {
-
-  // }
-
-  // Remember to call logApiToSupabase() where it's needed in your component
-
-  const downloadConversationHistory = async (courseName: string) => {
-    try {
-      setIsLoading(true)
-      const response = await axios.get(
-        `https://flask-production-751b.up.railway.app/export-convo-history-csv?course_name=${courseName}`,
-        { responseType: 'blob' },
-      )
-      const url = window.URL.createObjectURL(new Blob([response.data]))
-      const link = document.createElement('a')
-      link.href = url
-      link.setAttribute('download', courseName + '_conversation_history.csv')
-      document.body.appendChild(link)
-      link.click()
-    } catch (error) {
-      console.error('Error fetching conversation history:', error)
-      notifications.show({
-        id: 'error-notification',
-        title: 'Error',
-        message:
-          'Failed to fetch conversation history. Please try again later.',
-        color: 'red',
-        radius: 'lg',
-        icon: <IconAlertCircle />,
-        className: 'my-notification-class',
-        style: { backgroundColor: '#15162c' },
-        loading: false,
-      })
-    } finally {
-      setIsLoading(false)
-    }
   }
 
   return (
@@ -390,300 +286,11 @@ const MakeToolsPage = ({ course_name }: { course_name: string }) => {
                 {isLoading ? 'Saving...' : 'Save'}
               </Button>
             </div>
-
-            <div className="pt-5"></div>
-            {/* NOMIC VISUALIZATION  */}
-            {/* {false ? ( */}
-            {/* {true ? ( */}
-            {/* {nomicIsLoading ? (
-              <>
-                <span className="nomic-iframe skeleton-box pl-7 pr-7 pt-4"></span>
-              </>
-            ) : (
-              <>
-                <iframe
-                  className="nomic-iframe pl-7 pr-7 pt-4 pt-4"
-                  // id={nomicMapData.map_id}
-                  allow="clipboard-read; clipboard-write"
-                  src={'https://primary-production-60d0.up.railway.app/setup'}
-                />
-                <Title
-                  order={6}
-                  className={`w-full text-center ${montserrat_heading.variable} mt-2 font-montserratHeading`}
-                >
-                  A conceptual flow chart of the pre and post processing of
-                  context for the LLM.
-                  <br></br>
-                  Read more about{' '}
-                  <a
-                    className={'text-purple-600'}
-                    href="https://docs.n8n.io/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ textDecoration: 'underline', paddingRight: '5px' }}
-                  >
-                    n8n
-                  </a>
-                </Title>
-              </>
-            )} */}
           </Flex>
         </div>
         <GlobalFooter />
       </main>
     </>
-  )
-}
-
-import {
-  IconAlertCircle,
-  IconCheck,
-  IconCloudDownload,
-  IconDownload,
-  IconExternalLink,
-} from '@tabler/icons-react'
-
-import { CannotEditCourse } from './CannotEditCourse'
-import { type CourseMetadata } from '~/types/courseMetadata'
-// import { CannotViewCourse } from './CannotViewCourse'
-
-interface CourseFile {
-  name: string
-  s3_path: string
-  course_name: string
-  readable_filename: string
-  type: string
-  url: string
-  base_url: string
-}
-
-interface CourseFilesListProps {
-  files: CourseFile[]
-}
-import { IconTrash } from '@tabler/icons-react'
-import { MainPageBackground } from './MainPageBackground'
-import { extractEmailsFromClerk } from './clerkHelpers'
-import { notifications } from '@mantine/notifications'
-import GlobalFooter from './GlobalFooter'
-import Navbar from './navbars/Navbar'
-import { N8nWorkflowsTable } from './N8nWorkflowsTable'
-
-const CourseFilesList = ({ files }: CourseFilesListProps) => {
-  const router = useRouter()
-  const { classes, theme } = useStyles()
-  const handleDelete = async (s3_path: string, course_name: string) => {
-    try {
-      const response = await axios.delete(
-        `https://flask-production-751b.up.railway.app/delete`,
-        {
-          params: { s3_path, course_name },
-        },
-      )
-      // Handle successful deletion, show a success message
-      showToastOnFileDeleted(theme)
-      // Refresh the page
-      await router.push(router.asPath)
-    } catch (error) {
-      console.error(error)
-      // Show error message
-      showToastOnFileDeleted(theme, true)
-    }
-  }
-
-  return (
-    <div
-      className="mx-auto w-full justify-center rounded-md  bg-violet-100 p-5 shadow-md" // bg-violet-100
-      style={{ marginTop: '-1rem', backgroundColor: '#0F1116' }}
-    >
-      <ul role="list" className="grid grid-cols-2 gap-4">
-        {files.map((file, index) => (
-          <li
-            key={file.s3_path}
-            className="hover:shadow-xs flex items-center justify-between gap-x-6 rounded-xl bg-violet-300 py-4 pl-4 pr-1 transition duration-200 ease-in-out hover:bg-violet-200 hover:shadow-violet-200"
-            onMouseEnter={(e) => {
-              // Removed this because it causes the UI to jump around on mouse enter.
-              // e.currentTarget.style.border = 'solid 1.5px'
-              e.currentTarget.style.borderColor = theme.colors.violet[8]
-            }}
-            onMouseLeave={(e) => {
-              // e.currentTarget.style.border = 'solid 1.5px'
-            }}
-          >
-            {/* Conditionally show link in small text if exists */}
-            {file.url ? (
-              <div
-                className="min-w-0 flex-auto"
-                style={{
-                  maxWidth: '100%',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                <a href={file.url} target="_blank" rel="noopener noreferrer">
-                  <p className="truncate text-xl font-semibold leading-6 text-gray-800">
-                    {file.readable_filename}
-                  </p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-600">
-                    {file.url || ''}
-                  </p>
-                </a>
-              </div>
-            ) : (
-              <div
-                className="min-w-0 flex-auto"
-                style={{
-                  maxWidth: '80%',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                <p className="text-xl font-semibold leading-6 text-gray-800">
-                  {file.readable_filename}
-                </p>
-                {/* SMALL LOWER TEXT FOR FILES IN LIST */}
-                {/* <p className="mt-1 truncate text-xs leading-5 text-gray-600">
-                  {file.course_name}
-                </p> */}
-              </div>
-            )}
-            <div className="me-4 flex justify-end space-x-2">
-              {/* Download button */}
-              <button
-                onClick={() =>
-                  fetchPresignedUrl(file.s3_path).then((url) => {
-                    if (url) {
-                      window.open(url, '_blank')
-                    }
-                  })
-                }
-                className="btn-circle btn cursor-pointer items-center justify-center border-0 bg-transparent transition duration-200 ease-in-out"
-                // style={{ outline: 'solid 1px', outlineColor: 'white' }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.backgroundColor = theme.colors.grape[8]
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colorScheme === 'dark'
-                      ? theme.colors.gray[2]
-                      : theme.colors.gray[1]
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.backgroundColor = 'transparent'
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colors.gray[8]
-                }}
-              >
-                <IconDownload className="h-5 w-5 text-gray-800" />
-              </button>
-              {/* Delete button */}
-              <button
-                onClick={() =>
-                  handleDelete(
-                    file.s3_path as string,
-                    file.course_name as string,
-                  )
-                }
-                className="btn-circle btn cursor-pointer items-center justify-center border-0 bg-transparent transition duration-200 ease-in-out"
-                // style={{ outline: 'solid 1px', outlineColor: theme.white }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.backgroundColor = theme.colors.grape[8]
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colorScheme === 'dark'
-                      ? theme.colors.gray[2]
-                      : theme.colors.gray[1]
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.backgroundColor = 'transparent'
-                  ;(e.currentTarget.children[0] as HTMLElement).style.color =
-                    theme.colors.red[6]
-                }}
-              >
-                <IconTrash className="h-5 w-5 text-red-600" />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-async function fetchCourseMetadata(course_name: string) {
-  try {
-    const response = await fetch(
-      `/api/UIUC-api/getCourseMetadata?course_name=${course_name}`,
-    )
-    console.log('REsponse received while fetching metadata:', response)
-    if (response.ok) {
-      const data = await response.json()
-      if (data.success === false) {
-        throw new Error(
-          data.message || 'An error occurred while fetching course metadata',
-        )
-      }
-      // Parse is_private field from string to boolean
-      if (
-        data.course_metadata &&
-        typeof data.course_metadata.is_private === 'string'
-      ) {
-        data.course_metadata.is_private =
-          data.course_metadata.is_private.toLowerCase() === 'true'
-      }
-      return data.course_metadata
-    } else {
-      throw new Error(
-        `Error fetching course metadata: ${
-          response.statusText || response.status
-        }`,
-      )
-    }
-  } catch (error) {
-    console.error('Error fetching course metadata:', error)
-    throw error
-  }
-}
-
-const showToastOnFileDeleted = (theme: MantineTheme, was_error = false) => {
-  return (
-    // docs: https://mantine.dev/others/notifications/
-
-    notifications.show({
-      id: 'file-deleted-from-materials',
-      withCloseButton: true,
-      onClose: () => console.log('unmounted'),
-      onOpen: () => console.log('mounted'),
-      autoClose: 6000,
-      // position="top-center",
-      title: was_error ? 'Error deleting file' : 'Deleting file...',
-      message: was_error
-        ? "An error occurred while deleting the file. Please try again and I'd be so grateful if you email kvday2@illinois.edu to report this bug."
-        : 'The file will be delted in the background. After about 10 seconds, it will be 100% purged from our servers and, of course, will no longer be used by the chatbot.',
-      icon: <IconCheck />,
-      // className: 'my-notification-class',
-      styles: {
-        root: {
-          backgroundColor: was_error
-            ? theme.colors.errorBackground
-            : theme.colors.nearlyWhite,
-          borderColor: was_error
-            ? theme.colors.errorBorder
-            : theme.colors.aiPurple,
-        },
-        title: {
-          color: theme.colors.nearlyBlack,
-        },
-        description: {
-          color: theme.colors.nearlyBlack,
-        },
-        closeButton: {
-          color: theme.colors.nearlyBlack,
-          '&:hover': {
-            backgroundColor: theme.colors.dark[1],
-          },
-        },
-      },
-      loading: false,
-    })
   )
 }
 
