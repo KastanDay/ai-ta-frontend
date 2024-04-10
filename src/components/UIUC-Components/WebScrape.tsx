73eb72bd1d3ca640041c02efb3787884ec3d712e
Maxwell-Lindsey <mal13@illinois.edu>
2024-04-10 17:10:57 -0500

    refactored mit ocw ingest; need to test when it is live again on backend

--- src/components/UIUC-Components/WebScrape.tsx
+++ src/components/UIUC-Components/WebScrape.tsx
@@ -399,16 +399,11 @@ export const WebScrape = ({
     try {
       if (!url || !courseName || !localDir) return null
       console.log('calling downloadMITCourse')
-      const response = await axios.get(
-        `https://flask-production-751b.up.railway.app/mit-download`,
-        {
-          params: {
-            url: url,
-            course_name: courseName,
-            local_dir: localDir,
-          },
-        },
-      )
+      const response = await axios.post('/api/mitIngest', {
+        url,
+        courseName,
+        localDir,
+      });
       return response.data
     } catch (error) {
       console.error('Error during MIT course download:', error)
