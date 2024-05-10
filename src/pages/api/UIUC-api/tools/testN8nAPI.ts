76a5bf7cb88651886ef98e9edade8c0a84946faa
Kastan Day <kastanvday@gmail.com>
2024-05-10 11:21:59 -0700

    Fix 'clear N8n api key' functionality

--- src/pages/api/UIUC-api/tools/testN8nAPI.ts
+++ src/pages/api/UIUC-api/tools/testN8nAPI.ts
@@ -18,5 +18,6 @@ export default async function handler(req: any, res: any) {
     // return res.status(response.status).json({ error: response.statusText })
     throw new Error(`Unable to fetch n8n tools: ${response.statusText}`)
   }
+  return res.status(200).json({ message: 'Success' })
   // console.log('Fetch was ok. ', await response.json())
 }
