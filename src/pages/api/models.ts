4321c2742c3a85032bf1b177077eb0fbd3f5e004
Kastan Day <kastanvday@gmail.com>
2024-07-15 15:25:38 -0700

    merging

--- src/pages/api/models.ts
+++ src/pages/api/models.ts
@@ -8,16 +8,26 @@ import {
 
 import { OpenAIModel, OpenAIModelID, OpenAIModels } from '@/types/openai'
 import { decrypt, isEncrypted } from '~/utils/crypto'
-import { LLMProvider, ProviderNames, SupportedModels } from '~/types/LLMProvider'
+import {
+  LLMProvider,
+  ProviderNames,
+  SupportedModels,
+} from '~/types/LLMProvider'
 import { getOllamaModels, runOllamaChat } from '~/utils/modelProviders/ollama'
 import { getOpenAIModels } from '~/utils/modelProviders/openai'
 import { getAzureModels } from '~/utils/modelProviders/azure'
-import { getAnthropicModels, runAnthropicChat } from '~/utils/modelProviders/anthropic'
+import {
+  getAnthropicModels,
+  runAnthropicChat,
+} from '~/utils/modelProviders/anthropic'
 
 import { WebllmModel } from '~/utils/modelProviders/WebLLM'
-import { ModelRecord, prebuiltAppConfig } from '~/utils/modelProviders/ConfigWebLLM'
+import {
+  ModelRecord,
+  prebuiltAppConfig,
+} from '~/utils/modelProviders/ConfigWebLLM'
 import { OllamaModel } from '~/utils/modelProviders/ollama'
-import { CreateMLCEngine } from "@mlc-ai/web-llm";
+import { CreateMLCEngine } from '@mlc-ai/web-llm'
 import { ChevronsDownLeft } from 'tabler-icons-react'
 import { env } from 'process'
 
@@ -30,25 +40,24 @@ export function convertToLocalModels(record: ModelRecord): WebllmModel {
     name: record.model_id,
     parameterSize: 'Unknown',
     tokenLimit: record.overrides?.context_window_size,
-    downloadSize: record.vram_required_MB ? `${(record.vram_required_MB / 1024).toFixed(2)}GB` : 'unknown',
-  };
+    downloadSize: record.vram_required_MB
+      ? `${(record.vram_required_MB / 1024).toFixed(2)}GB`
+      : 'unknown',
+  }
 }
-export const webLLMModels: WebllmModel[] = prebuiltAppConfig.model_list.map((model: ModelRecord) => convertToLocalModels(model));
+export const webLLMModels: WebllmModel[] = prebuiltAppConfig.model_list.map(
+  (model: ModelRecord) => convertToLocalModels(model),
+)
 export let ollamaModels: OllamaModel[] = []
 
 const handler = async (req: Request): Promise<Response> => {
-  console.log('in handler')
-  let apiKey = ''
-  let apiType = OPENAI_API_TYPE
-  let endpoint = OPENAI_API_HOST
-  console.log('this is what reuest is', req)
   try {
-    const { key } = (await req.json()) as {
-      key: string
 
-    }
-    console.log('key', key)
+    // TODO: MOVE THESE TO DB INPUTS
 
+    // const { key } = (await req.json()) as {
+    //   key: string
+    // }
     // Eventually we'll use this. For now, there's no API Key for Ollama
     const ollamaProvider: LLMProvider = {
       provider: ProviderNames.Ollama,
@@ -61,77 +70,54 @@ const handler = async (req: Request): Promise<Response> => {
       enabled: true,
       apiKey: process.env.OPENAI_API_KEY,
       baseUrl: 'https://ollama.ncsa.ai/api/tags',
-
     }
     const AzureProvider: LLMProvider = {
       provider: ProviderNames.Azure,
       enabled: true,
-      apiKey: process.env.AZURE_API_KEY,   // this is the azure api key
+      apiKey: process.env.AZURE_API_KEY, // this is the azure api key
       AzureDeployment: 'gpt-35-turbo-16k',
-      AzureEndpoint: 'https://uiuc-chat-canada-east.openai.azure.com/'
-
+      AzureEndpoint: 'https://uiuc-chat-canada-east.openai.azure.com/',
     }
 
     const AnthropicProvider: LLMProvider = {
       provider: ProviderNames.Anthropic,
       enabled: true,
-      apiKey: process.env.ANTHROPIC_API_KEY,   // this is the anthropic api key
-      AnthropicModel: 'claude-3-opus-20240229'
+      apiKey: process.env.ANTHROPIC_API_KEY, // this is the anthropic api key
+      AnthropicModel: 'claude-3-opus-20240229',
       //AzureEndpoint: 'https://uiuc-chat-canada-east.openai.azure.com/'
-
     }
 
-    const llmProviderKeys: LLMProvider[] = [ollamaProvider, OpenAIProvider, AzureProvider, AnthropicProvider]
-    // iterates and collects all models for the givne provider keys
-    let totalModels: SupportedModels[] = []
-    for (const provider of llmProviderKeys) {
-      if (provider.provider == 'Ollama') {
+    const llmProviderKeys: LLMProvider[] = [
+      ollamaProvider,
+      OpenAIProvider,
+      AzureProvider,
+      AnthropicProvider,
+    ]
+    // END-TODO: MOVE THESE TO DB INPUTS
+
+    let allSupportedModels: { [providerName: string]: SupportedModels } = {}
+    for (const llmProvider of llmProviderKeys) {
+      if (llmProvider.provider == ProviderNames.Ollama) {
         const fetchedOllamaModels = await getOllamaModels(ollamaProvider)
         ollamaModels = fetchedOllamaModels // Update the exported variable
-        totalModels.push(ollamaModels)
-      }
-      else if (provider.provider == 'OpenAI') {
-        //2. call an endpoint to check which openai modle available
+        allSupportedModels[llmProvider.provider] = fetchedOllamaModels
+      } else if (llmProvider.provider == ProviderNames.OpenAI) {
         const openAIModels = await getOpenAIModels(OpenAIProvider)
-        totalModels.push(openAIModels)
-      }
-      else if (provider.provider == 'Azure') {
-        const azureOpenaiModels = await getAzureModels(AzureProvider)
-        totalModels.push(azureOpenaiModels)
-      }
-      else if (provider.provider == 'Anthropic') {
-        const AnthropicModels = await getAnthropicModels(AnthropicProvider)
-        totalModels.push(AnthropicModels)
-      }
-      else {
+        allSupportedModels[llmProvider.provider] = openAIModels
+      } else if (llmProvider.provider == ProviderNames.Azure) {
+        const azureModels = await getAzureModels(AzureProvider)
+        allSupportedModels[llmProvider.provider] = azureModels
+      } else if (llmProvider.provider == ProviderNames.Anthropic) {
+        const anthropicModels = await getAnthropicModels(AnthropicProvider)
+        allSupportedModels[llmProvider.provider] = anthropicModels
+      } else {
         continue
       }
-
     }
-    //print all models to terminal
-    console.log('total models available', totalModels)
 
-    // checking Anthropic chat function
-    console.log('entering Anthropic chat')
-    runAnthropicChat(AnthropicProvider)
+    console.log('allSupportedModels', allSupportedModels)
 
-    // Test chat function
-    //const ret = await runOllamaChat() still needs work fors stremaing
-
-    // legacy code take a look
-    apiKey = key ? key : (process.env.OPENAI_API_KEY as string)
-    // Check if the key starts with 'sk-' (indicating it's not encrypted)
-    if (key && isEncrypted(key)) {
-      // Decrypt the key
-      const decryptedText = await decrypt(
-        key,
-        process.env.NEXT_PUBLIC_SIGNING_KEY as string,
-      )
-      apiKey = decryptedText as string
-    }
-    console.log('models.ts Final openai key: ', apiKey)
-    // return total models which compiles all valid models
-    return new Response(JSON.stringify(totalModels), { status: 200 })
+    return new Response(JSON.stringify(allSupportedModels), { status: 200 })
   } catch (error) {
     console.error(error)
     return new Response('Error', { status: 500 })
