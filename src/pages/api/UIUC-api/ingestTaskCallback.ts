571403be46b96a1336b9ed97dac4b1b4f1f8a481
Kastan Day <kastanvday@gmail.com>
2024-07-18 16:13:04 -0700

    HOTFIX: Beam task callback made robust to all failure cases

--- src/pages/api/UIUC-api/ingestTaskCallback.ts
+++ src/pages/api/UIUC-api/ingestTaskCallback.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '~/utils/supabaseClient'
-import posthog from 'posthog-js'
 
 // The beam function handles sending to Success and Failure cases. We just handle removing from in-progress.
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -18,60 +17,39 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     // OR:
     // Data: { error: 'Task timed out' }
 
-    // If Beam task is FAILED or TIMEOUT (not success)
+    // If failure_ingest or Beam task is FAILED or TIMEOUT
     if (
+      data.failure_ingest ||
       req.headers['x-beam-task-status'] === 'FAILED' ||
       req.headers['x-beam-task-status'] === 'TIMEOUT'
     ) {
       console.log('in IF for Task FAILED or TIMEOUT')
       // Remove from in progress, add to failed
-      const { data, error: delErr } = await supabase
+      const { data: record, error: delErr } = await supabase
         .from('documents_in_progress')
         .delete()
         .eq('beam_task_id', req.headers['beam-task-id'])
         .select()
-      // remove a few fields from data, the beam_task_id is not needed
-      delete data![0].beam_task_id
-      delete data![0].created_at
-      data![0].error = req.body.error
+      // remove a few fields from record, the beam_task_id is not needed
+      delete record![0].beam_task_id
+      delete record![0].created_at
 
-      const { error: insertErr } = await supabase
-        .from('documents_failed')
-        .insert(data![0])
+      // Add error message, either specific one or "timeout" from Beam body.
+      record![0].error = data.failure_ingest.error || req.body.error
+
+      await supabase.from('documents_failed').insert(record![0])
 
       return res.status(200).json({ message: 'Success' })
     }
-
     // beam-task-status is SUCCESS (but ingest could have still failed)
-    let s3_path_completed = ''
-    if (data.success_ingest) {
-      console.log('IN SUCCESS INGEST ___:', data.success_ingest)
-      s3_path_completed = data.success_ingest
-    }
-    if (data.failure_ingest) {
-      s3_path_completed = data.failure_ingest.s3_path
-    }
-
-    // Remove from in progress
-    if (s3_path_completed) {
-      const { error } = await supabase
+    else if (data.success_ingest) {
+      console.debug('IN SUCCESS INGEST ___:', data.success_ingest)
+      const { data: record, error: delErr } = await supabase
         .from('documents_in_progress')
         .delete()
-        .eq('s3_path', s3_path_completed)
-
-      console.log('Deleted from documents_in_progress: ', data.success_ingest)
-      if (error) {
-        console.error(
-          '❌❌ Supabase failed to delete from `documents_in_progress`:',
-          error,
-        )
-        posthog.capture('supabase_failure_delete_documents_in_progress', {
-          s3_path: s3_path_completed,
-          error: error.message,
-        })
-      }
+        .eq('beam_task_id', req.headers['beam-task-id'])
     } else {
-      console.error(`No success/failure ingest in Beam callback data: ${data}}`)
+      console.error(`No success/failure ingest in Beam callback data: ${data}`)
     }
 
     return res.status(200).json({ message: 'Success' })
