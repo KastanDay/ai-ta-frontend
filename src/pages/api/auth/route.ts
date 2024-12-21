24845a99c2b803fea7088f913225ddd52d1fa55c
Kastan Day <kastanvday@gmail.com>
2024-12-21 11:14:09 -0800

    Make BetterAuth compatible with Next.js Pages router, thanks to https://github.com/better-auth/better-auth/issues/920

--- src/pages/api/auth/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { toNodeHandler } from "better-auth/node"
-import { auth } from "@/lib/auth"
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await toNodeHandler(auth)(req, res)
-}
